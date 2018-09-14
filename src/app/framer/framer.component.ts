7d445eba33cbc54e5b8c7144f628f2a56a1b74d5
Patrick Michalina <patrickmichalina@mac.com>
2018-09-14 16:18:59 -0500

    cleanup

--- src/app/framer/framer.component.ts
+++ src/app/framer/framer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core'
 
 const fill = (num: number) => Array(num).fill(0).map((_, i) => i + 1)
+const redNested = (num: number) => fill(num).reduce((acc, _curr, idx) => [...acc, fill(idx)], [])
 
 @Component({
   selector: 'app-framer',
@@ -8,6 +9,5 @@ const fill = (num: number) => Array(num).fill(0).map((_, i) => i + 1)
   styleUrls: ['./framer.component.scss']
 })
 export class FramerComponent {
-  readonly components = fill(10)
-  readonly containers = this.components.reduce((acc, _curr, idx) => [...acc, fill(idx)], [])
+  readonly containers = redNested(10)
 }
