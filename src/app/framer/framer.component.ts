6d1c582432ea5ca22326537a072ed8ae50e54121
Patrick Michalina <patrickmichalina@mac.com>
2018-10-01 18:15:13 -0500

    updates

--- src/app/framer/framer.component.ts
+++ src/app/framer/framer.component.ts
@@ -1,26 +1,31 @@
-import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormControl } from '@angular/forms'
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core'
+import { FormGroup, FormControl, Validators } from '@angular/forms'
 import { distinctUntilChanged } from 'rxjs/operators'
 
 const fill = (num: number) => Array(num).fill(0).map((_, i) => i + 1)
 const redNested = (num: number) => fill(num).reduce((acc, _curr, idx) => [...acc, fill(idx)], [])
+const objectsAreEqual = (a: any) => (b: any) => JSON.stringify(a) === JSON.stringify(b)
 
 const MAX_HEIGHT_START_VALUE = 900
+const ELEMENT_COUNT_START_VALUE = 4
 
 @Component({
   selector: 'app-framer',
   templateUrl: './framer.component.html',
-  styleUrls: ['./framer.component.scss']
+  styleUrls: ['./framer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FramerComponent implements OnInit {
-  readonly containers = redNested(10)
+  readonly containers = redNested(ELEMENT_COUNT_START_VALUE)
+
   readonly formGroup = new FormGroup({
-    maxHeight: new FormControl(MAX_HEIGHT_START_VALUE, [])
+    maxHeight: new FormControl(MAX_HEIGHT_START_VALUE, [Validators.required]),
+    elementCount: new FormControl(ELEMENT_COUNT_START_VALUE, [Validators.required])
   })
 
   ngOnInit() {
     this.formGroup.valueChanges
-      .pipe(distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)))
+      .pipe(distinctUntilChanged((a, b) => objectsAreEqual(a)(b)))
       .subscribe(console.log)
   }
 }
