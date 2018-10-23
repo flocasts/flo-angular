59ac2f6352e70883d7d7456c09941cbbf4bf7a43
Patrick Michalina <patrickmichalina@mac.com>
2018-10-23 11:03:59 -0500

    fix test deps

--- src/app/pwa.service.spec.ts
+++ src/app/pwa.service.spec.ts
@@ -1,8 +1,13 @@
 import { TestBed } from '@angular/core/testing'
 import { PwaService } from './pwa.service'
+import { ServiceWorkerModule } from '@angular/service-worker'
 
 describe('PwaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}))
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      ServiceWorkerModule.register('')
+    ]
+  }))
 
   it('should be created', () => {
     const service: PwaService = TestBed.get(PwaService)
