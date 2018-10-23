b8a224995230d52358a6fc06cff6ef46176d17bb
Patrick Michalina <patrickmichalina@mac.com>
2018-10-22 21:12:32 -0500

    fixed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,14 +1,12 @@
 import { AppPage } from './app.po'
 
 describe('App', () => {
-  // tslint:disable-next-line:no-let
-  let page: AppPage
-
-  beforeEach(() => {
-    page = new AppPage()
-  })
-
-  it('should display welcome message', () => {
-    expect(page.getParagraphText()).toEqual('FloSports Angular')
+  it('should display welcome message', done => {
+    const page = new AppPage()
+    page.navigateTo()
+      .then(() => {
+        expect(page.getParagraphText()).toEqual('FloSports Angular')
+        done()
+      })
   })
 })
