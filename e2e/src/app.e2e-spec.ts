26e62ba6fbbd5bde5672777b289d26a2cf54942b
Patrick Michalina <patrickmichalina@mac.com>
2018-10-22 21:12:50 -0500

    fix

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -3,7 +3,8 @@ import { AppPage } from './app.po'
 describe('App', () => {
   it('should display welcome message', done => {
     const page = new AppPage()
-    page.navigateTo()
+    page
+      .navigateTo()
       .then(() => {
         expect(page.getParagraphText()).toEqual('FloSports Angular')
         done()
