99d8ddcac5f26046436de86a5e8d2170b8e00bdb
Patrick Michalina <patrickmichalina@mac.com>
2018-11-03 11:05:50 -0500

    tests

--- projects/flosportsinc/ng-viewport-grid/src/lib/viewport-grid-box.component.spec.ts
+++ projects/flosportsinc/ng-viewport-grid/src/lib/viewport-grid-box.component.spec.ts
@@ -1,16 +1,41 @@
 import { async, TestBed } from '@angular/core/testing'
 import { ViewportGridBoxComponent } from './viewport-grid-box.component'
+import { Component, NgModule } from '@angular/core'
+import { By } from '@angular/platform-browser'
+import { take } from 'rxjs/operators'
+
+@Component({
+  selector: 'flo-test-component',
+  template: `
+  <flo-viewport-grid-box>
+    <div floViewportGridBoxItem></div>
+  </flo-viewport-grid-box>
+  `
+})
+export class TestComponent {
+}
+
+@NgModule({
+  declarations: [TestComponent, ViewportGridBoxComponent],
+  exports: [TestComponent]
+})
+export class TestingModule { }
 
 const createSut = () => {
-  const hoist = TestBed.createComponent<ViewportGridBoxComponent<HTMLDivElement>>(ViewportGridBoxComponent)
+  const hoist = TestBed.createComponent<TestComponent>(TestComponent)
   hoist.autoDetectChanges()
-  return hoist
+  const directive = hoist.debugElement.query(By.directive(ViewportGridBoxComponent))
+  return {
+    hoist,
+    directive,
+    instance: directive.injector.get<ViewportGridBoxComponent<HTMLDivElement>>(ViewportGridBoxComponent)
+  }
 }
 
 describe(ViewportGridBoxComponent.name, () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ViewportGridBoxComponent]
+      imports: [TestingModule]
     }).compileComponents()
   }))
 
@@ -19,16 +44,16 @@ describe(ViewportGridBoxComponent.name, () => {
   })
 
   it('should have guid', () => {
-    expect(createSut().componentInstance.guid.length).toEqual(12)
+    expect(createSut().instance.guid.length).toEqual(12)
   })
 
   it('should have guid set on id', () => {
-    expect(createSut().nativeElement.id.length).toEqual(12)
+    expect(createSut().directive.nativeElement.id.length).toEqual(12)
   })
 
   it('should toggle to selected', () => {
     const sut = createSut()
-    const instance = sut.componentInstance
+    const instance = sut.instance
 
     instance.setSelected(false)
     const spy = spyOn(instance, 'setSelected')
@@ -38,7 +63,7 @@ describe(ViewportGridBoxComponent.name, () => {
 
   it('should toggle to not-selected', () => {
     const sut = createSut()
-    const instance = sut.componentInstance
+    const instance = sut.instance
 
     instance.setSelected(true)
     const spy = spyOn(instance, 'setSelected')
@@ -48,17 +73,51 @@ describe(ViewportGridBoxComponent.name, () => {
 
   it('should add the border class when selected', () => {
     const sut = createSut()
-    const instance = sut.componentInstance
 
-    const spy = spyOn(instance, 'setSelected')
-    // instance()
+    sut.instance.setSelected(true)
+    const elmsWithClass = sut.directive.queryAll(By.css('div.flo-vp-selected'))
+    expect(elmsWithClass.length).toEqual(1)
   })
 
   it('should remove the border class when selection is lost', () => {
     const sut = createSut()
-    const instance = sut.componentInstance
 
-    const spy = spyOn(instance, 'setSelected')
-    // instance()
+    sut.instance.setSelected(false)
+    const elmsWithClass = sut.directive.queryAll(By.css('div.flo-vp-selected'))
+    expect(elmsWithClass.length).toEqual(0)
+  })
+
+  it('should emit isSelected$ event when selected', done => {
+    const sut = createSut()
+
+    sut.instance.isSelected$.pipe(take(1)).subscribe(evt => {
+      expect(evt).toEqual(true)
+      done()
+    })
+
+    sut.instance.setSelected(true)
+  })
+
+  it('should emit isSelected$ event when deselected', done => {
+    const sut = createSut()
+
+    sut.instance.isSelected$.pipe(take(1)).subscribe(evt => {
+      expect(evt).toEqual(false)
+      done()
+    })
+
+    sut.instance.setSelected(false)
+  })
+
+  it('should pass through click event', done => {
+    const sut = createSut()
+
+    sut.instance.clicked$.pipe(take(1)).subscribe(evt => {
+      expect(evt instanceof ViewportGridBoxComponent).toEqual(true)
+      done()
+    })
+
+    const elm = sut.directive.nativeElement as HTMLDivElement
+    elm.click()
   })
 })
