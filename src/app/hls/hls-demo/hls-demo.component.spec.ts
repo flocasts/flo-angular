7008bbb3c9261516c6d05e70b77e47f0dde8c2fa
Patrick Michalina <patrickmichalina@mac.com>
2018-11-13 20:27:42 -0600

    wip

--- src/app/hls/hls-demo/hls-demo.component.spec.ts
+++ src/app/hls/hls-demo/hls-demo.component.spec.ts
@@ -1,12 +1,12 @@
 import { async, TestBed } from '@angular/core/testing'
 import { HlsDemoComponent } from './hls-demo.component'
 import { SharedTestingModule } from '../../shared.testing.module'
-import { HlsJsModule } from '@flosportsinc/ng-media-source-extensions'
+import { HlsModule } from '@flosportsinc/ng-media-source-extensions'
 
 describe('HlsDemoComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [SharedTestingModule, HlsJsModule],
+      imports: [SharedTestingModule, HlsModule],
       declarations: [HlsDemoComponent]
     }).compileComponents()
   }))
