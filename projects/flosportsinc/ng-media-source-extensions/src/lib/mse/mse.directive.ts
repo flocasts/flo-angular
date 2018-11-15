d8b6ca90d91aef1a333b8d2608495601975f5ae9
Patrick Michalina <patrickmichalina@mac.com>
2018-11-14 22:23:35 -0600

    cleanup

--- projects/flosportsinc/ng-media-source-extensions/src/lib/mse/mse.directive.ts
+++ projects/flosportsinc/ng-media-source-extensions/src/lib/mse/mse.directive.ts
@@ -11,8 +11,8 @@ import {
 } from '@angular/core'
 import {
   SUPPORTS_TARGET_VIA_MEDIA_SOURCE_EXTENSION, SUPPORTS_MSE_TARGET_NATIVELY, IVideoElementSupportsTargetMseCheck,
-  MEDIA_SOURCE_EXTENSION_LIBRARY_INIT_TASK, MEDIA_SOURCE_EXTENSION_LIBRARY_DESTROY_TASK, IMseInitFunc,
-  IMseDestroyFunc, MEDIA_SOURCE_EXTENSION_LIBRARY_SRC_CHANGE_TASK, IMseSrcChangeFunc, IMseDestroy, IMseInit, IMseSrcChange
+  MEDIA_SOURCE_EXTENSION_LIBRARY_INIT_TASK, MEDIA_SOURCE_EXTENSION_LIBRARY_DESTROY_TASK,
+  MEDIA_SOURCE_EXTENSION_LIBRARY_SRC_CHANGE_TASK, IMseDestroy, IMseInit, IMseSrcChange
 } from './mse.tokens'
 import { filter, map, takeUntil, take, skip } from 'rxjs/operators'
 import { Subject, combineLatest } from 'rxjs'
@@ -35,30 +35,25 @@ export interface IMseClientReadyEvent<TMseClient> {
 })
 export class MseDirective<TMseClient, TMseMessage> implements OnDestroy, OnChanges, AfterViewInit {
   // tslint:disable:readonly-array
-  constructor(readonly el: ElementRef<HTMLVideoElement>,
+  constructor(readonly _elementRef: ElementRef<HTMLVideoElement>,
     @Inject(SUPPORTS_TARGET_VIA_MEDIA_SOURCE_EXTENSION) private readonly _isMediaSourceSupported: boolean,
     @Inject(SUPPORTS_MSE_TARGET_NATIVELY) private readonly _nativeSupportCheck: IVideoElementSupportsTargetMseCheck,
     @Inject(MEDIA_SOURCE_EXTENSION_LIBRARY_INIT_TASK) private readonly _mseInitTask: IMseInit<TMseClient, TMseMessage>[],
     @Inject(MEDIA_SOURCE_EXTENSION_LIBRARY_SRC_CHANGE_TASK) private readonly _mseSourceChangeTask: IMseSrcChange<TMseClient>[],
     @Inject(MEDIA_SOURCE_EXTENSION_LIBRARY_DESTROY_TASK) private readonly _mseDestroyTask: IMseDestroy<TMseClient>[]
-  ) {
-    console.log(this)
-   }
-
-  // public
+  ) { }
 
   private readonly _ngOnDestroy$ = new Subject<undefined>()
   private readonly _ngAfterViewInit$ = new Subject<undefined>()
   private readonly _srcChanges$ = new Subject<string>()
   private readonly _mseClientSource$ = new Subject<TMseClient>()
   private readonly _mseClientMessages$ = new Subject<TMseMessage>()
-  public readonly videoElement = this.el.nativeElement
+  public readonly videoElement = this._elementRef.nativeElement
 
   @Input() public readonly src?: string
   @Output() public readonly srcChange = this._srcChanges$.asObservable().pipe(takeUntil(this._ngOnDestroy$))
   @Output() public readonly mseClient = this._mseClientSource$.asObservable().pipe(takeUntil(this._ngOnDestroy$))
   @Output() public readonly mseClientMessage = this._mseClientMessages$.asObservable().pipe(takeUntil(this._ngOnDestroy$))
-  // @Output() public readonly errors = this._errorSource$.asObservable().pipe(takeUntil(this._ngOnDestroy$))
 
   public ngAfterViewInit() {
     emitAndUnsubscribe(this._ngAfterViewInit$)
@@ -99,7 +94,6 @@ export class MseDirective<TMseClient, TMseMessage> implements OnDestroy, OnChang
     this.videoElement.setAttribute('src', src)
   })
 
-
   private readonly _mediaSourceClientPathSourceChangeSubscription = combineLatest(
     this._mseClientSupported$,
     this._srcChanges$,
@@ -119,7 +113,6 @@ export class MseDirective<TMseClient, TMseMessage> implements OnDestroy, OnChang
   private readonly _mseClientPathInitSubscription = combineLatest(this._mseClientSupported$, this._srcChanges$)
     .pipe(map(res => res[1]), take(1))
     .subscribe(src => {
-      // console.log(this, this._mseInitTask)
       const mseClient = this._mseInitTask[0].func({
         src,
         videoElement: this.videoElement,
