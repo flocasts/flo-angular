3eec9c85c8f6e84dd5861da41ffaf68f62e2657e
Patrick Michalina <patrickmichalina@mac.com>
2018-12-21 13:14:51 -0600

    tets

--- projects/flosportsinc/ng-media-source-extensions-dash/src/lib/dash.module.spec.ts
+++ projects/flosportsinc/ng-media-source-extensions-dash/src/lib/dash.module.spec.ts
@@ -1,22 +1,19 @@
-import { TestBed, async } from '@angular/core/testing'
+import { TestBed } from '@angular/core/testing'
 import {
-  IMseSrcChangeOptions, SUPPORTS_TARGET_VIA_MEDIA_SOURCE_EXTENSION,
-  SUPPORTS_MSE_TARGET_NATIVELY,
-  MseDirective,
-  IMseInit,
-  IMseInitOptions
+  IMseSrcChangeOptions,
+  IMseInitOptions,
+  IMseDestroyOptions
 } from '@flosportsinc/ng-media-source-extensions'
-import { SimpleChange, Component, Input, NgModule } from '@angular/core'
-import { take } from 'rxjs/operators'
-import { Subject, ObjectUnsubscribedError } from 'rxjs'
-import { By } from '@angular/platform-browser'
+import { Component, Input, NgModule } from '@angular/core'
+import { Subject } from 'rxjs'
 import {
   DashModule, defaultDashSupportedNativelyFunction,
-  defaultDashIsSupportedFactory, defaultDashClientSrcChangeFunction, defaultDashPatternCheck, DashMessage, defaultDashClientInitFunction
+  defaultDashIsSupportedFactory, defaultDashClientSrcChangeFunction, defaultDashPatternCheck,
+  DashMessage, defaultDashClientInitFunction, defaultDashClientDestroyFunction
 } from './dash.module'
 import { MediaPlayerClass, MediaPlayer } from 'dashjs'
 
-const TEST_SRC = 'http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8'
+const TEST_SRC = 'http://www.streambox.fr/playlists/x36xhzz/x36xhzz.mpd'
 
 @Component({
   selector: 'flo-test-component',
@@ -33,78 +30,6 @@ export class DashTestComponent {
   exports: [DashTestComponent]
 })
 export class HlsTestingModule { }
-
-// const createSut = () => {
-//   const hoist = TestBed.createComponent(DashTestComponent)
-//   hoist.autoDetectChanges()
-//   const directive = hoist.debugElement.query(By.directive(MseDirective))
-//   return {
-//     hoist,
-//     directive,
-//     instance: directive.injector.get(MseDirective)
-//   }
-// }
-
-// const setTestBed = (supportsMle: boolean) => (native: boolean) => {
-//   TestBed.configureTestingModule({
-//     imports: [HlsTestingModule],
-//     providers: [
-//       {
-//         provide: SUPPORTS_TARGET_VIA_MEDIA_SOURCE_EXTENSION,
-//         useValue: supportsMle,
-//         multi: true
-//       },
-//       {
-//         provide: SUPPORTS_MSE_TARGET_NATIVELY,
-//         useValue: () => native,
-//         multi: true
-//       }
-//     ]
-//   })
-// }
-
-// const shouldUnsubscribeFromInternalNgOnDestroy = async(() => {
-//   const wrapper = createSut()
-//   const internalNgOnDestroy$ = (wrapper.instance as any)._ngOnDestroy$ as Subject<undefined>
-
-//   internalNgOnDestroy$.pipe(take(1)).subscribe(response => {
-//     expect(response).toBeUndefined()
-//   })
-
-//   wrapper.hoist.destroy()
-
-//   expect(() => internalNgOnDestroy$.next()).toThrow(new ObjectUnsubscribedError())
-// })
-
-// const shouldUnsubscribeFromInternalNgAfterViewInit = async(() => {
-//   const wrapper = createSut()
-//   const internalNgAfterViewInit$ = (wrapper.instance as any)._ngAfterViewInit$ as Subject<undefined>
-
-//   expect(() => {
-//     internalNgAfterViewInit$.pipe(take(1)).subscribe()
-//   }).toThrow(new ObjectUnsubscribedError())
-
-//   wrapper.hoist.destroy()
-// })
-
-// const shouldCompileTestComponent = done => {
-//   expect(createSut().hoist).toBeDefined()
-//   done()
-// }
-
-// const shouldCompilerDirective = done => {
-//   expect(createSut().directive).toBeDefined()
-//   done()
-// }
-
-// const skipSrcChangeWhenValueIs = (sc: SimpleChange) => {
-//   const wrapper = createSut()
-//   const spy = spyOn((wrapper.instance as any)._srcChanges$, 'next')
-//   wrapper.instance.ngOnChanges({
-//     floHls: sc
-//   })
-//   expect(spy).not.toHaveBeenCalled()
-// }
 
 describe(DashModule.name, () => {
   it('should construct', () => {
@@ -153,6 +78,22 @@ describe(DashModule.name, () => {
     it('when default test environment', () => {
       expect(defaultDashIsSupportedFactory().func()).toEqual(true)
     })
+
+    it('when no mse, return false', () => {
+      // tslint:disable-next-line:no-object-mutation
+      (window as any).MediaSource = undefined
+      expect(defaultDashIsSupportedFactory().func()).toEqual(false)
+    })
+
+    it('when WebKitMediaSource return true', () => {
+      // tslint:disable-next-line:no-object-mutation
+      (window as any).MediaSource = undefined;
+      // tslint:disable-next-line:no-object-mutation
+      (window as any).WebKitMediaSource = () => {
+        // noop
+      }
+      expect(defaultDashIsSupportedFactory().func()).toEqual(true)
+    })
   })
 
   describe(`exposed ${defaultDashClientSrcChangeFunction.name} factory function`, () => {
@@ -169,46 +110,19 @@ describe(DashModule.name, () => {
     })
   })
 
-  // describe(`when client supports Media Source Extensions`, () => {
-  //   beforeEach(() => setTestBed(true)(false))
-  //   afterEach(() => TestBed.resetTestingModule())
-
-  //   it('should compile the test component', shouldCompileTestComponent)
-  //   it('should compile the directive under test', shouldCompilerDirective)
-
-  //   it('should not trigger MSE source change when same src string', done => {
-  //     const wrapper = createSut()
-  //     const spy = spyOn(wrapper.instance as any, '_mseSourceChangeTask')
-  //     wrapper.instance.ngOnChanges({
-  //       src: new SimpleChange(TEST_SRC, 'http://www.streambox.fr/playlists/x36xhzz/x36xhzz.m3u8', false)
-  //     })
-  //     expect(spy).not.toHaveBeenCalled()
-  //     done()
-  //   })
-
-  //   it('should skip src change when value is same', () => {
-  //     skipSrcChangeWhenValueIs(new SimpleChange(TEST_SRC, TEST_SRC, false))
-  //   })
-
-  //   it('should skip src change when value is undefined', () => {
-  //     skipSrcChangeWhenValueIs(new SimpleChange(undefined, undefined, false))
-  //   })
-
-  //   it('should unsubscribe from internal ngOnDestroy$ subject after single event emission', shouldUnsubscribeFromInternalNgOnDestroy)
-  //   it('should unsubscribe from internal ngAfterViewInit$ subject after single event emission',
-  //     shouldUnsubscribeFromInternalNgAfterViewInit)
-  // })
-
-  // describe(`when supports mse client natively`, () => {
-  //   beforeEach(() => setTestBed(false)(true))
-  //   afterEach(() => TestBed.resetTestingModule())
-
-  //   it('should compile the test component', shouldCompileTestComponent)
-  //   it('should compile the directive under test', shouldCompilerDirective)
-  //   it('should unsubscribe from internal ngOnDestroy$ subject after single event emission', shouldUnsubscribeFromInternalNgOnDestroy)
-  //   it('should unsubscribe from internal ngAfterViewInit$ subject after single event emission',
-  //     shouldUnsubscribeFromInternalNgAfterViewInit)
-  // })
+  it('should execute defaultDashClientDestroyFunction correctly', () => {
+    const videoElement = document.createElement('video')
+    videoElement.setAttribute('autoplay', 'true')
+    const event: IMseDestroyOptions<MediaPlayerClass> = { clientRef: MediaPlayer().create(), videoElement }
+    const spy1 = spyOn(videoElement, 'pause')
+    const spy2 = spyOn(event.clientRef, 'reset')
+    defaultDashClientDestroyFunction().func({
+      clientRef: event.clientRef,
+      videoElement
+    })
+    expect(spy1).toHaveBeenCalled()
+    expect(spy2).toHaveBeenCalled()
+  })
 
   it('should default pattern', () => {
     expect(defaultDashPatternCheck().func('something.mpd')).toEqual(true)
