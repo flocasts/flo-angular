6678668bf80f6731e2a0645ab3c11413165a7b11
Patrick Michalina <patrickmichalina@mac.com>
2018-12-26 15:53:37 -0600

    fix(npm): remove files attribute in package (#63)

--- tools/publish.ts
+++ tools/publish.ts
@@ -1,29 +1,44 @@
 import { maybe } from 'typescript-monads'
-import { PROJECTS } from './libs'
 import { exec } from 'shelljs'
+import { PROJECTS } from './libs'
 import { resolve } from 'path'
-import { readFileSync, writeFileSync } from 'fs'
+import { readFile, writeFile } from 'fs'
+import { bindNodeCallback, forkJoin, EMPTY } from 'rxjs'
+import { map, flatMap } from 'rxjs/operators'
 
-maybe(process.argv.find(a => a.includes('ver=')))
-  .map(a => a.replace('ver=', ''))
-  .tapSome(version => {
-    PROJECTS
-      .map(proj => resolve(proj.replace('@', 'dist/')))
-      .forEach(path => {
-        try {
-          const packagePath = resolve(path, 'package.json')
-          const file = JSON.parse(readFileSync(packagePath, 'utf-8').toString()) as any
-          const newFile = {
-            ...file,
-            version
-          }
-          writeFileSync(packagePath, JSON.stringify(newFile, undefined, 2), 'utf-8')
-          exec(`npm publish ${path} --access=public`)
-        } catch (err) {
-          console.error(err)
-          process.exit(1)
-        }
-      })
-  })
+const readFile_ = (path: string) => bindNodeCallback(readFile)(path)
+const writeFile_ = (path: string) => (data: string) => bindNodeCallback(writeFile)(path, data)
+const findVer = (a: string) => a.includes('ver=')
+const cleanVer = (a: string) => a.replace('ver=', '')
+const resolvePackageJson = (path: string) => resolve(path, 'package.json')
+const execNpmPublish = (path: string) => exec(`npm publish ${path} --access=public`)
+const execNpmPublisCollection = (paths: ReadonlyArray<string>) => paths.forEach(execNpmPublish)
 
+const mapToProjectObject =
+  (path: string) => ({ path, packagePath: resolvePackageJson(path) })
 
+const mapToVerPack =
+  (projectPaths: ReadonlyArray<string>) =>
+    (version: string) =>
+      ({ version, projects: projectPaths.map(mapToProjectObject) })
+
+const pPaths = PROJECTS.map(proj => resolve(proj.replace('@', 'dist/')))
+
+const catchError = (err: any) => {
+  console.error(err)
+  process.exit(1)
+}
+
+maybe(process.argv.find(findVer))
+  .map(cleanVer)
+  .map(mapToVerPack(pPaths))
+  .map(a => a.projects.map(proj => readFile_(proj.packagePath).pipe(
+    map(buffer => JSON.parse(buffer.toString())),
+    map(file => ({ ...proj, file, version: a.version })),
+    flatMap(res => writeFile_(res.packagePath)(JSON.stringify({ ...res.file, version: res.version }, undefined, 2)).pipe(
+      map(() => res.path)
+    ))
+  )))
+  .map(a => forkJoin(a))
+  .valueOr(EMPTY)
+  .subscribe(execNpmPublisCollection, catchError)
