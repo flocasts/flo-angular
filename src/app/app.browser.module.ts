1a5cab8acce9422041c4537cb00bd2e0d07906dd
Patrick Michalina <patrickmichalina@mac.com>
2018-12-27 15:38:33 -0600

    fix(ad-block): cache initial response

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component'
 import { DashModule } from '@flosportsinc/ng-media-source-extensions-dash'
 import { HlsModule } from '@flosportsinc/ng-media-source-extensions-hls'
 import { WindowBrowserModule } from '@flosportsinc/ng-universal-services/src/window'
+import { AdBlockBrowserModule, AdBlockService } from '@flosportsinc/ng-universal-services/src/ad-block'
 
 @NgModule({
   imports: [
     HlsModule,
     DashModule,
     AppModule,
+    AdBlockBrowserModule.usingUrl('/assets/ads.js'),
     WindowBrowserModule.withWindowObject()
   ],
   providers: [
