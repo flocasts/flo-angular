6685d4368902485269df4475a272e0511f1c76c4
Patrick Michalina <patrickmichalina@mac.com>
2019-01-11 13:13:16 -0600

    use font-awesome

--- tools/libs.ts
+++ tools/libs.ts
@@ -4,6 +4,18 @@ const notIncludes = (searchString: string) => (a: string) => !a.includes(searchS
 
 export const PROJECTS = Object.keys(config.compilerOptions.paths).filter(notIncludes('/*'))
 
+const FONT_AWESOME_TO_EXTRACT: ReadonlyArray<any> = [
+  'check',
+  'minus',
+  'search',
+  'times',
+  'cc-amex',
+  'cc-visa',
+  'cc-discovery',
+  'cc-mastercard'
+]
+
 export const PROJECT_ASSET_COPY_DICT = {
+  [`node_modules/@fortawesome/fontawesome-free/svgs/**/*(${FONT_AWESOME_TO_EXTRACT.join('|')}).*`]: 'dist/flosportsinc/ng-icons/svg',
   'projects/flosportsinc/ng-icons/src/lib/svg/**/*.*': 'dist/flosportsinc/ng-icons/svg'
 }
