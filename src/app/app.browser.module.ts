d80bd57f354be822bd4e432c4a9f48538014702d
Patrick Michalina <patrickmichalina@mac.com>
2019-01-14 18:22:55 -0600

    lift packages out of source

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module'
 import { AppComponent } from './app.component'
 import { DashModule } from '@flosportsinc/ng-media-source-extensions-dash'
 import { HlsModule } from '@flosportsinc/ng-media-source-extensions-hls'
-import { WindowBrowserModule } from '@flosportsinc/ng-universal-services/src/window'
-import { AdBlockBrowserModule } from '@flosportsinc/ng-universal-services/src/ad-block'
+import { WindowBrowserModule } from '@flosportsinc/ng-universal-services/window'
+import { AdBlockBrowserModule } from '@flosportsinc/ng-universal-services/ad-block'
 import { NodeEnvTransferBrowserModule } from '@flosportsinc/ng-universal-services/node-env-transfer/browser'
 
 @NgModule({
--- src/app/pwa.service.spec.ts
+++ src/app/pwa.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing'
 import { PwaService } from './pwa.service'
 import { ServiceWorkerModule } from '@angular/service-worker'
-import { WINDOW } from '@flosportsinc/ng-universal-services/src/window'
+import { WINDOW } from '@flosportsinc/ng-universal-services/window'
 
 describe('PwaService', () => {
   beforeEach(() => TestBed.configureTestingModule({
