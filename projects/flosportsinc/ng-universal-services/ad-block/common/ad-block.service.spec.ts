c985bdbef4f7065c9f530cc023a09fb70f54f7ba
Patrick Michalina <patrickmichalina@mac.com>
2019-01-15 18:34:13 -0600

    tests

--- projects/flosportsinc/ng-universal-services/ad-block/common/ad-block.service.spec.ts
+++ projects/flosportsinc/ng-universal-services/ad-block/common/ad-block.service.spec.ts
@@ -1,67 +1,17 @@
 import { TestBed } from '@angular/core/testing'
+import { AdBlockModule } from './ad-block.module'
 import { AdBlockService } from './ad-block.service'
-import { AdBlockBrowserModule } from '../browser/ad-block.browser.module'
-import { AdBlockServerModule } from '../server/ad-block.server.module'
-import { NgModule } from '@angular/core'
-import { take } from 'rxjs/operators'
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
 
-@NgModule({
-  imports: [
-    HttpClientTestingModule,
-    AdBlockBrowserModule.usingUrl('http://mysite.com/ads.js')
-  ]
-})
-export class AdBlockBrowserTestModule { }
-
-@NgModule({
-  imports: [
-    HttpClientTestingModule,
-    AdBlockServerModule
-  ]
-})
-export class AdBlockServerTestModule { }
-
-
-const getService = () => TestBed.get(AdBlockService) as AdBlockService
-const getHttpMock = () => TestBed.get(HttpTestingController) as HttpTestingController
-
-describe('Ad-Block Module', () => {
+describe(AdBlockModule.name, () => {
   afterEach(() => TestBed.resetTestingModule())
 
-  describe('when on server platform', () => {
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [AdBlockServerTestModule]
-      })
-    })
-    it('should not run GET request and always return false', () => {
-      getService().isAnAdBlockerActive().pipe(take(1)).subscribe(res => {
-        expect(res).toEqual(false)
-      })
-      getHttpMock().expectNone('http://mysite.com/ads.js')
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AdBlockModule]
     })
   })
-  describe('when on browser platform', () => {
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [AdBlockBrowserTestModule]
-      })
-    })
-    it('should return true when http request is blocked', () => {
-      getService().isAnAdBlockerActive().pipe(take(1)).subscribe(res => {
-        expect(res).toEqual(true)
-      })
-      const mock = getHttpMock().expectOne('http://mysite.com/ads.js')
-      mock.error({} as any)
-    })
 
-    it('should return false when http request returns success', () => {
-      getService().isAnAdBlockerActive().pipe(take(1)).subscribe(res => {
-        expect(res).toEqual(false)
-      })
-      const mock = getHttpMock().expectOne('http://mysite.com/ads.js')
-      mock.flush({})
-    })
+  it('should construct', () => {
+    expect(() => TestBed.get(AdBlockService)).toThrowError()
   })
 })
