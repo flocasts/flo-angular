fd3c08043e8bcff9a58c010ad957b4b4daa326e6
Patrick Michalina <patrickmichalina@mac.com>
2019-01-22 21:56:26 -0600

    catch error for node readFile

--- projects/flosportsinc/ng-universal-services/svg-transfer-state/server/svg-transfer-state.server.service.ts
+++ projects/flosportsinc/ng-universal-services/svg-transfer-state/server/svg-transfer-state.server.service.ts
@@ -1,18 +1,19 @@
-import { Injectable, Inject } from '@angular/core'
-import { bindNodeCallback } from 'rxjs'
-import { tap, map } from 'rxjs/operators'
-import { readFile } from 'fs'
-import { SVG_TRANSFER_KEY, SVG_LOADER_HTTP_REQUEST } from './svg-transfer-state.tokens'
-import { ISvgLoaderService, ISvgLoaderHttpFunc } from './svg-transfer-state.interfaces'
+import { SVG_TRANSFER_KEY, SVG_LOADER_HTTP_REQUEST, SVG_LOADER_ERROR_RETURN_OPERATOR } from './svg-transfer-state.tokens'
+import { ISvgLoaderService, ISvgLoaderHttpFunc, ISvgLoaderErrorReturnValueStreamFunc } from './svg-transfer-state.interfaces'
 import { TransferState, makeStateKey } from '@angular/platform-browser'
 import { SVG_REQUEST_PATTERN } from './svg-transfer-state.tokens'
 import { ISvgRequestPatternFunc } from './svg-transfer-state.interfaces'
+import { Injectable, Inject } from '@angular/core'
+import { bindNodeCallback } from 'rxjs'
+import { tap, map, catchError } from 'rxjs/operators'
+import { readFile } from 'fs'
 
 @Injectable()
 export class SvgServerLoaderService implements ISvgLoaderService {
   constructor(private _ts: TransferState,
     @Inject(SVG_REQUEST_PATTERN) private _reqPattern: ISvgRequestPatternFunc,
-    @Inject(SVG_LOADER_HTTP_REQUEST) private _httpRequest: ISvgLoaderHttpFunc) { }
+    @Inject(SVG_LOADER_HTTP_REQUEST) private _httpRequest: ISvgLoaderHttpFunc,
+    @Inject(SVG_LOADER_ERROR_RETURN_OPERATOR) private _errorHandler: ISvgLoaderErrorReturnValueStreamFunc) { }
 
   readonly load = (svgKey: string) =>
     svgKey.includes('://')
@@ -20,6 +21,7 @@ export class SvgServerLoaderService implements ISvgLoaderService {
       : bindNodeCallback(readFile)(this._reqPattern(svgKey))
         .pipe(
           map(a => a.toString()),
+          catchError(err => this._errorHandler(err)),
           tap(this.cacheForBrowserReflow(svgKey)))
 
   readonly cacheForBrowserReflow = (svgKey: string) => (svg: string) => {
