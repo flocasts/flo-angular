8926afb82a6cd803812e60c34c409992a6e4c259
Patrick Michalina <patrickmichalina@mac.com>
2019-01-25 16:53:37 -0600

    remove cache

--- src/app/app.browser.module.ts
+++ src/app/app.browser.module.ts
@@ -17,7 +17,7 @@ import { SvgTransferStateBrowserModule } from '@flosportsinc/ng-universal-servic
     HlsModule,
     DashModule,
     AppModule,
-    SvgTransferStateBrowserModule.withConfig({ cacheMaxAge: 1000 * 60 * 60 * 24 * 7 })
+    SvgTransferStateBrowserModule
   ],
   providers: [
     PwaService
