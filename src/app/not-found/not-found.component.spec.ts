13b06ec4f0494a60616f5a70a3777f7ffd2c1c73
Patrick Michalina <patrickmichalina@mac.com>
2019-01-30 19:37:16 -0600

    fix te

--- src/app/not-found/not-found.component.spec.ts
+++ src/app/not-found/not-found.component.spec.ts
@@ -1,9 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { NotFoundComponent } from './not-found.component'
+import { MarkdownModule } from 'ngx-markdown'
 
 describe('NotFoundComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MarkdownModule],
       declarations: [NotFoundComponent]
     }).compileComponents()
   }))
