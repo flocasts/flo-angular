c78925a6698879f4b0191ee4cb81efafc37bc5ae
Patrick Michalina <patrickmichalina@mac.com>
2019-02-07 10:21:04 -0600

    fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,9 +6,4 @@ import { Component, ChangeDetectionStrategy, Inject } from '@angular/core'
   styleUrls: ['./home.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HomeComponent {
-
-  constructor() {
-    // import('//imasdk.googleapis.com/js/sdkloader/ima3.js').then(console.log)
-  }
-}
+export class HomeComponent { }
