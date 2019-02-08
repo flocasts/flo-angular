6c0da5124895cde04d83db58f99dd0d510c2d164
Patrick Michalina <patrickmichalina@mac.com>
2019-02-07 23:34:01 -0600

    stats

--- projects/flosportsinc/ng-media-source-extensions/karma.conf.js
+++ projects/flosportsinc/ng-media-source-extensions/karma.conf.js
@@ -2,8 +2,8 @@ const config = require('../../../src/karma.conf.shared')
 module.exports = config('../coverage')([
   { pattern: 'test/**/*.*', included: false, served: true }
 ])({
-  statements: 90,
-  lines: 90,
+  statements: 80,
+  lines: 80,
   branches: 100,
-  functions: 85
+  functions: 80
 })
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,2 +1,8 @@
 const config = require('./karma.conf.shared')
-module.exports = config('../coverage/flo-angular')()()
+module.exports = config('../coverage/flo-angular')([
+])({
+  statements: 90,
+  lines: 100,
+  branches: 90,
+  functions: 90
+})
