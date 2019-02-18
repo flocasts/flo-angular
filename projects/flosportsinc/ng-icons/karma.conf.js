83cf5a22518b3d37cd5d4a20b0f12b8b9edeeeaf
Patrick Michalina <patrickmichalina@mac.com>
2019-02-18 17:42:43 -0600

    tests

--- projects/flosportsinc/ng-icons/karma.conf.js
+++ projects/flosportsinc/ng-icons/karma.conf.js
@@ -1,2 +1,7 @@
 const config = require('../../../src/karma.conf.shared')
-module.exports = config('../coverage/ng-icons')()()
+module.exports = config('../coverage/ng-icons')()({
+  statements: 100,
+  lines: 100,
+  branches: 100,
+  functions: 50
+})
