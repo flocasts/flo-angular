e6076041a16ebf4ce35e8f5e4f03eb187a6a2d59
Patrick Michalina <patrickmichalina@mac.com>
2019-02-20 22:57:39 -0600

    chore: update project dependencies (#116)

--- tools/compress.ts
+++ tools/compress.ts
@@ -1,6 +1,5 @@
-import { createGzip } from 'zlib'
+const zlib = require('zlib') // brotli types not provided yet
 import { createReadStream, createWriteStream } from 'fs'
-import { compressStream } from 'iltorb'
 import * as glob from 'glob'
 
 const compressFile =
@@ -18,7 +17,7 @@ const compressFile =
 
 glob('dist/flo-angular/**/*!(*.br|*.gz).{js,css,html,txt,json}', (_error, matches) => {
   matches.forEach(match => {
-    compressFile(match, createGzip, 'gz')
-    compressFile(match, compressStream, 'br')
+    compressFile(match, zlib.createGzip, 'gz')
+    compressFile(match, zlib.createBrotliCompress, 'br')
   })
 })
