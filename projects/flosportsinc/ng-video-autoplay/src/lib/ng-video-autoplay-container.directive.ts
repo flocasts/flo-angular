46863bba8e3cdf7818564da486119094e1240565
Patrick Michalina <patrickmichalina@mac.com>
2019-02-28 17:15:31 -0600

    lint

--- projects/flosportsinc/ng-video-autoplay/src/lib/ng-video-autoplay-container.directive.ts
+++ projects/flosportsinc/ng-video-autoplay/src/lib/ng-video-autoplay-container.directive.ts
@@ -7,6 +7,7 @@ import { VideoAutoplayDirective } from './ng-video-autoplay.directive'
 export class VideoAutoplayContainerDirective  {
   @ContentChild(VideoAutoplayDirective) public readonly video?: ElementRef<any>
 
+  // tslint:disable-next-line:use-life-cycle-interface
   ngAfterContentInit() {
     console.log('TEST')
     console.log(this.video)
