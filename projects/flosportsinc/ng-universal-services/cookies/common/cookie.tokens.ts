7d6bfb52489f176cf116c03d954312cb825484ee
Patrick Michalina <patrickmichalina@mac.com>
2019-04-23 22:47:38 -0500

    fix

--- projects/flosportsinc/ng-universal-services/cookies/common/cookie.tokens.ts
+++ projects/flosportsinc/ng-universal-services/cookies/common/cookie.tokens.ts
@@ -1,4 +1,3 @@
 import { Type } from '@angular/core'
-import { ICookieService } from '../common/cookie.interfaces'
 
-export const COOKIE_SERVICE = 'flo.cookie.service' as unknown as Type<ICookieService>
+export const COOKIE_SERVICE = 'flo.cookie.service' as unknown as Type<any>
