d622ba108e2634ca90072ff99a10a03d070a9e48
Patrick Michalina <patrickmichalina@mac.com>
2019-04-28 00:35:19 -0500

    fix

--- projects/flosportsinc/ng-ad-block/common/ad-block.service.ts
+++ projects/flosportsinc/ng-ad-block/common/ad-block.service.ts
@@ -1,19 +1,14 @@
-import { Injectable } from '@angular/core'
+import { Injectable, Inject } from '@angular/core'
 import { IAdBlockService } from './ad-block.interface'
 import { AD_BLOCK_LOADER } from './ad-block.tokens'
 import { Observable } from 'rxjs'
 
 @Injectable({
-  providedIn: 'root',
-  deps: [AD_BLOCK_LOADER],
-  useFactory
+  providedIn: 'root'
 })
 export class AdBlockService implements IAdBlockService {
-  constructor(private _loader: Observable<boolean>) { }
+  constructor(@Inject(AD_BLOCK_LOADER) private _loader: Observable<boolean>) { }
 
   readonly isAnAdBlockerActive = () => this._loader
 }
 
-export function useFactory(loader: Observable<boolean>) {
-  return new AdBlockService(loader)
-}
