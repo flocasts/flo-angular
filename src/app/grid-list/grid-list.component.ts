1b061b4e4dac019f67e380219f1790b211fdaa71
Patrick Michalina <patrickmichalina@mac.com>
2019-05-09 20:37:59 -0500

    lint fail

--- src/app/grid-list/grid-list.component.ts
+++ src/app/grid-list/grid-list.component.ts
@@ -7,7 +7,8 @@ import { Component, ChangeDetectionStrategy } from '@angular/core'
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GridListComponent {
-  public readonly items: ReadonlyArray<any> = [
+  // tslint:disable-next-line: readonly-keyword
+  public items: ReadonlyArray<any> = [
     { id: '123', prop: '123' } as any,
     { id: '456', prop: '456' },
     { id: '789', prop: '789' },
