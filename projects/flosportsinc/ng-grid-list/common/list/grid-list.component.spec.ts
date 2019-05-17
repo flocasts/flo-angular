f2a133a5edbe3b0b63fdcda171e498758e0aed5f
Patrick Michalina <patrickmichalina@mac.com>
2019-05-17 16:56:19 -0500

    tests

--- projects/flosportsinc/ng-grid-list/common/list/grid-list.component.spec.ts
+++ projects/flosportsinc/ng-grid-list/common/list/grid-list.component.spec.ts
@@ -4,10 +4,14 @@ import { Component, NgModule, ViewChild } from '@angular/core'
 import { FloGridListModule } from '../ng-grid-list.module'
 import { FloGridListViewComponent } from '../grid/grid.component'
 import { maybe } from 'typescript-monads'
+import { By } from '@angular/platform-browser'
 
 // tslint:disable: readonly-keyword
 // tslint:disable: no-object-mutation
 // tslint:disable: readonly-array
+
+const SAMPLE_ITEM_1 = { id: 'id1', src: 'https://cdn-flo.flodogs.com/uploaded/mzEp5zBJ943XvZXba7DnVmvD2zZPqJk6/playlist.m3u8' }
+const SAMPLE_ITEM_2 = { id: 'id2', src: 'https://cdn-flo.flowrestling.org/uploaded/G3op3lQBy394D561vB33By4ZWB2dqD7V/playlist.m3u8' }
 
 @Component({
   selector: 'flo-grid-list-test-component',
@@ -25,8 +29,8 @@ export class FloGridListTestComponent {
   @ViewChild(FloGridListComponent) _listRef: FloGridListComponent<any>
   @ViewChild(FloGridListViewComponent) _gridRef: FloGridListViewComponent<any>
   items = [
-    { id: 'id1', src: 'https://cdn-flo.flodogs.com/uploaded/mzEp5zBJ943XvZXba7DnVmvD2zZPqJk6/playlist.m3u8' },
-    { id: 'id2', src: 'https://cdn-flo.flowrestling.org/uploaded/G3op3lQBy394D561vB33By4ZWB2dqD7V/playlist.m3u8' },
+    SAMPLE_ITEM_1,
+    SAMPLE_ITEM_2,
     { id: 'id3', src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4' },
     { id: 'id4', src: 'https://cdn-flo.flowrestling.org/uploaded/xnJpnlmxVj0ZX3d17gLJDPaewJ2dYBv3/playlist.m3u8' },
     { id: 'id5', src: 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4' }
@@ -43,10 +47,14 @@ export class FloGridTestingModule { }
 
 const createSut = () => {
   const hoistFixture = TestBed.createComponent(FloGridListTestComponent)
+  const gridListDebug = hoistFixture.debugElement.query(By.directive(FloGridListComponent))
+  const gridDebug = hoistFixture.debugElement.query(By.directive(FloGridListViewComponent))
   hoistFixture.detectChanges()
   return {
     hoistFixture,
     hoistInstance: hoistFixture.componentInstance,
+    gridListDebug,
+    gridDebug
   }
 }
 
@@ -64,11 +72,11 @@ describe(FloGridListComponent.name, () => {
   })
 
   describe('viewItems', () => {
+    noop() // coverage
     it('should return default empty item when not connected to grid', () => {
       const sut = createSut()
       spyOn(sut.hoistInstance._listRef, 'maybeGridRef').and.callFake(() => maybe())
       sut.hoistFixture.detectChanges()
-      noop() // coverage
       Object.values(sut.hoistInstance._listRef.viewItems[0].roles).forEach(val => expect(val).toEqual(false))
       Object.values(sut.hoistInstance._listRef.viewItems[0].actions).forEach(action => expect(action).toEqual(noop))
     })
@@ -91,25 +99,39 @@ describe(FloGridListComponent.name, () => {
     it('should call autofill', () => {
       const sut = createSut()
       const spy = spyOn(sut.hoistInstance._listRef, 'autoFill').and.callThrough()
-      sut.hoistFixture.detectChanges()
       sut.hoistInstance.autoFillOnLoad = true
       sut.hoistFixture.detectChanges()
+      sut.hoistInstance._listRef.ngOnInit()
       expect(sut.hoistInstance._listRef.autoFillOnLoad).toEqual(true)
-      // expect(spy).toHaveBeenCalled()
+      expect(spy).toHaveBeenCalled()
     })
   })
 
-  // describe('initialFill', () => {
-  //   it('should pass input', () => {
-  //     const sut = createSut()
-  //     sut.hoistInstance.initialFill = { 1: 'id5' }
-  //     sut.hoistFixture.detectChanges()
-  //     expect(sut.hoistInstance._listRef.initialFill).toEqual(sut.hoistInstance.initialFill)
-  //   })
-  //   it('should fill', () => {
-  //     const sut = createSut()
-  //     sut.hoistInstance.initialFill = { 1: 'id5' }
-  //     sut.hoistFixture.detectChanges()
-  //   })
-  // })
+  describe('initialFill', () => {
+    it('should pass input', () => {
+      const sut = createSut()
+      sut.hoistInstance.initialFill = { 1: 'id5' }
+      sut.hoistFixture.detectChanges()
+      expect(sut.hoistInstance._listRef.initialFill).toEqual(sut.hoistInstance.initialFill)
+    })
+    it('should fill', () => {
+      const sut = createSut()
+      const spy = spyOn(sut.hoistInstance._gridRef, 'setItems').and.callThrough()
+      sut.hoistInstance.initialFill = { 1: 'id1' }
+      sut.hoistFixture.detectChanges()
+      sut.hoistInstance._listRef.ngOnInit()
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith([undefined, SAMPLE_ITEM_1])
+    })
+
+    it('should fill n-times', () => {
+      const sut = createSut()
+      const spy = spyOn(sut.hoistInstance._gridRef, 'setItems').and.callThrough()
+      sut.hoistInstance.initialFill = { 0: 'id2', 1: 'id1' }
+      sut.hoistFixture.detectChanges()
+      sut.hoistInstance._listRef.ngOnInit()
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenCalledWith([SAMPLE_ITEM_2, SAMPLE_ITEM_1])
+    })
+  })
 })
