26a3232d10489f819c5e746dc81567e5fa61ad02
Patrick Michalina <patrickmichalina@mac.com>
2019-05-17 22:51:26 -0500

    remove viewport grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
-import { ViewportGridModule } from '@flosportsinc/ng-viewport-grid'
 import { HomeComponent } from './home/home.component'
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { HlsComponent } from './hls/hls.component'
@@ -39,7 +38,6 @@ import { NodeEnvTransferModule, NodeEnvTransferService } from '@flosportsinc/ng-
     FormsModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    ViewportGridModule,
     TransferHttpCacheModule,
     HttpClientModule,
     SvgTransferStateModule,
