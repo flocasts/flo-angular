25fba5820fc22ff12fa227f7e06e114f76eaf7b9
Patrick Michalina <patrickmichalina@mac.com>
2019-06-09 19:38:03 -0500

    shorten tokens

--- projects/flosportsinc/ng-ad-block/common/ad-block.tokens.ts
+++ projects/flosportsinc/ng-ad-block/common/ad-block.tokens.ts
@@ -1,4 +1,4 @@
 import { InjectionToken } from '@angular/core'
 import { Observable } from 'rxjs'
 
-export const AD_BLOCK_LOADER = new InjectionToken<Observable<boolean>>('flo.ad-block-loader')
+export const AD_BLOCK_LOADER = new InjectionToken<Observable<boolean>>('fs.ab.loader')
--- projects/flosportsinc/ng-env-transfer-state/common/node-env-transfer.tokens.ts
+++ projects/flosportsinc/ng-env-transfer-state/common/node-env-transfer.tokens.ts
@@ -1,10 +1,10 @@
 import { Type } from '@angular/core'
 
-export const ENV = 'flo.node-env-transfer.config' as unknown as Type<any>
-export const ENV_CONFIG_DEFAULT = 'flo.node-env-transfer.config-browser-default' as unknown as Type<any>
-export const ENV_CONFIG_TS_KEY = 'flo.node-env-transfer.config-ts-key' as unknown as Type<any>
-export const NODE_ENV = 'flo.node-env-transfer.node-env' as unknown as Type<any>
-export const NODE_ENV_USE_VALUES = 'flo.node-env-transfer.node-env-def' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_EXTRACTOR = 'flo.node-env-transfer.config-extractor' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_REPLACER = 'flo.node-env-transfer.config-replacer' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_SELECTED = 'flo.node-env-transfer.config-selected' as unknown as Type<any>
+export const ENV = 'fs.net.config' as unknown as Type<any>
+export const ENV_CONFIG_DEFAULT = 'fs.net.config-browser-default' as unknown as Type<any>
+export const ENV_CONFIG_TS_KEY = 'fs.net.config-ts-key' as unknown as Type<any>
+export const NODE_ENV = 'fs.net.node-env' as unknown as Type<any>
+export const NODE_ENV_USE_VALUES = 'fs.net.node-env-def' as unknown as Type<any>
+export const ENV_CONFIG_SERVER_EXTRACTOR = 'fs.net.config-extractor' as unknown as Type<any>
+export const ENV_CONFIG_SERVER_REPLACER = 'fs.net.config-replacer' as unknown as Type<any>
+export const ENV_CONFIG_SERVER_SELECTED = 'fs.net.config-selected' as unknown as Type<any>
