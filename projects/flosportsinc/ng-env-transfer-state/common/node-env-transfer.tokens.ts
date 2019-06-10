b0fdbf872a68106818126342cecefd66d93b5244
Patrick Michalina <patrickmichalina@mac.com>
2019-06-09 20:59:36 -0500

    wip

--- projects/flosportsinc/ng-env-transfer-state/common/node-env-transfer.tokens.ts
+++ projects/flosportsinc/ng-env-transfer-state/common/node-env-transfer.tokens.ts
@@ -1,10 +1,10 @@
-import { Type } from '@angular/core'
+import { InjectionToken } from '@angular/core'
 
-export const ENV = 'fs.net.config' as unknown as Type<any>
-export const ENV_CONFIG_DEFAULT = 'fs.net.cfg-browser-def' as unknown as Type<any>
-export const ENV_CONFIG_TS_KEY = 'fs.net.cfg-ts-key' as unknown as Type<any>
-export const NODE_ENV = 'fs.net.node-env' as unknown as Type<any>
-export const NODE_ENV_USE_VALUES = 'fs.net.node-env-def' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_EXTRACTOR = 'fs.net.cfg-extractor' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_REPLACER = 'fs.net.cfg-replacer' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_SELECTED = 'fs.net.cfg-selected' as unknown as Type<any>
+export const ENV = new InjectionToken('fs.net.config')
+export const ENV_CONFIG_DEFAULT = new InjectionToken('fs.net.cfg-browser-def')
+export const ENV_CONFIG_TS_KEY = new InjectionToken('fs.net.cfg-ts-key')
+export const NODE_ENV = new InjectionToken('fs.net.node-env')
+export const NODE_ENV_USE_VALUES = new InjectionToken('fs.net.node-env-def')
+export const ENV_CONFIG_SERVER_EXTRACTOR = new InjectionToken('fs.net.cfg-extractor')
+export const ENV_CONFIG_SERVER_REPLACER = new InjectionToken('fs.net.cfg-replacer')
+export const ENV_CONFIG_SERVER_SELECTED = new InjectionToken('fs.net.cfg-selected')
