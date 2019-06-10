55c53b37f2552360b7fda30aa48f8be3c4c99fc8
Patrick Michalina <patrickmichalina@mac.com>
2019-06-09 20:10:07 -0500

    tokens

--- projects/flosportsinc/ng-env-transfer-state/common/node-env-transfer.tokens.ts
+++ projects/flosportsinc/ng-env-transfer-state/common/node-env-transfer.tokens.ts
@@ -1,10 +1,10 @@
 import { Type } from '@angular/core'
 
 export const ENV = 'fs.net.config' as unknown as Type<any>
-export const ENV_CONFIG_DEFAULT = 'fs.net.config-browser-default' as unknown as Type<any>
-export const ENV_CONFIG_TS_KEY = 'fs.net.config-ts-key' as unknown as Type<any>
+export const ENV_CONFIG_DEFAULT = 'fs.net.cfg-browser-def' as unknown as Type<any>
+export const ENV_CONFIG_TS_KEY = 'fs.net.cfg-ts-key' as unknown as Type<any>
 export const NODE_ENV = 'fs.net.node-env' as unknown as Type<any>
 export const NODE_ENV_USE_VALUES = 'fs.net.node-env-def' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_EXTRACTOR = 'fs.net.config-extractor' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_REPLACER = 'fs.net.config-replacer' as unknown as Type<any>
-export const ENV_CONFIG_SERVER_SELECTED = 'fs.net.config-selected' as unknown as Type<any>
+export const ENV_CONFIG_SERVER_EXTRACTOR = 'fs.net.cfg-extractor' as unknown as Type<any>
+export const ENV_CONFIG_SERVER_REPLACER = 'fs.net.cfg-replacer' as unknown as Type<any>
+export const ENV_CONFIG_SERVER_SELECTED = 'fs.net.cfg-selected' as unknown as Type<any>
