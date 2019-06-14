b47beaa981c92c42e116512cff4d3d7a850755ea
Patrick Michalina <patrickmichalina@mac.com>
2019-06-13 23:47:17 -0500

    wip

---
+++ projects/flosportsinc/ng-fullscreen/src/lib/ng-fullscreen.component.ts
@@ -0,0 +1,12 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core'
+
+@Component({
+  selector: 'flo-fullscreen',
+  templateUrl: './ng-fullscreen.component.html',
+  styleUrls: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FloFullscreenComponent {
+
+
+}
--- projects/flosportsinc/ng-fullscreen/src/lib/ng-fullscreen.module.ts
+++ projects/flosportsinc/ng-fullscreen/src/lib/ng-fullscreen.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { FloFullscreenDirective } from './ng-fullscreen.directive'
 import { FloFullscreenService } from './ng-fullscreen.service'
+import { FloFullscreenComponent } from './ng-fullscreen.component'
 
 @NgModule({
-  declarations: [FloFullscreenDirective],
-  exports: [FloFullscreenDirective],
+  declarations: [FloFullscreenDirective, FloFullscreenComponent],
+  exports: [FloFullscreenDirective, FloFullscreenComponent],
   providers: [FloFullscreenService]
 })
 export class FloFullscreenModule { }
