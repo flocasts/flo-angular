95c76df11dc4c79b46fde92f05e3887e73964f07
Patrick Michalina <patrickmichalina@mac.com>
2019-06-17 13:17:05 -0500

    fix: various AOT static config issues (#158)

--- projects/flosportsinc/ng-http-cache-tags/server/http-cache-tag.express.module.spec.ts
+++ projects/flosportsinc/ng-http-cache-tags/server/http-cache-tag.express.module.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing'
 import { HttpCacheTagInterceptor } from './http-cache-tag.interceptor'
 import { HttpCacheTagExpressServerModule, immutableAppend } from './http-cache-tag.express.module'
-import { CACHE_TAG_CONFIG, CACHE_TAG_WRITE_HEADER_FACTORY, IWriteResponseHeader, ICacheTagConfig } from './http-cache-tag.tokens'
-import { DEFAULT_CACHE_TAG_CONFIGURATION } from './http-cache-tag.module'
 import { RESPONSE } from '@nguniversal/express-engine/tokens'
+import { IHttpCacheTagServerModuleConfig } from './http-cache-tag.module'
+import {
+  CACHE_TAG_WRITE_HEADER_FACTORY, IWriteResponseHeader,
+  DEFAULT_CACHE_TAG_HEADER_KEY, CACHE_TAG_HEADER_KEY,
+  DEFAULT_CACHE_TAG_DELIMITER, CACHE_TAG_DELIMITER,
+  CACHE_TAG_RESPONSE_CODES, DEFAULT_CACHE_TAG_RESPONSE_CODES
+} from './http-cache-tag.tokens'
 
 describe(HttpCacheTagExpressServerModule.name, () => {
   afterEach(TestBed.resetTestingModule)
@@ -13,7 +18,7 @@ describe(HttpCacheTagExpressServerModule.name, () => {
     useValue: {
       getHeader: (key: string) => {
         const dict = {
-          [DEFAULT_CACHE_TAG_CONFIGURATION.headerKey]: 'Video-1'
+          [DEFAULT_CACHE_TAG_HEADER_KEY]: 'Video-1'
         }
         return dict[key]
       },
@@ -22,74 +27,74 @@ describe(HttpCacheTagExpressServerModule.name, () => {
   }
 
   const setupStandardTestBed = (config = {}) => TestBed.configureTestingModule({
-    imports: [HttpCacheTagExpressServerModule.withConfig(config)],
+    imports: [HttpCacheTagExpressServerModule.config(config)],
     providers: [RESPONSE_PROVIDER]
   })
 
   it('should construct with default values', () => {
     setupStandardTestBed()
     expect(TestBed.get(HttpCacheTagInterceptor)).toBeTruthy()
-    expect(TestBed.get(CACHE_TAG_CONFIG)).toEqual(DEFAULT_CACHE_TAG_CONFIGURATION)
+    expect(TestBed.get(CACHE_TAG_HEADER_KEY)).toEqual(DEFAULT_CACHE_TAG_HEADER_KEY)
+    expect(TestBed.get(CACHE_TAG_DELIMITER)).toEqual(DEFAULT_CACHE_TAG_DELIMITER)
+    expect(TestBed.get(CACHE_TAG_RESPONSE_CODES)).toEqual(DEFAULT_CACHE_TAG_RESPONSE_CODES)
   })
 
-  it('should construct withConfig empty', () => {
-    TestBed.configureTestingModule({
-      imports: [HttpCacheTagExpressServerModule.withConfig()],
-      providers: [RESPONSE_PROVIDER]
-    })
-    expect(TestBed.get(HttpCacheTagInterceptor)).toBeTruthy()
-    expect(TestBed.get(CACHE_TAG_CONFIG)).toEqual(DEFAULT_CACHE_TAG_CONFIGURATION)
+  it('should construct with partial', () => {
+    const config: Partial<IHttpCacheTagServerModuleConfig> = { }
+
+    setupStandardTestBed(config)
+    expect(TestBed.get(CACHE_TAG_RESPONSE_CODES)).toEqual(DEFAULT_CACHE_TAG_RESPONSE_CODES)
+    expect(TestBed.get(CACHE_TAG_HEADER_KEY)).toEqual(DEFAULT_CACHE_TAG_HEADER_KEY)
+    expect(TestBed.get(CACHE_TAG_DELIMITER)).toEqual(DEFAULT_CACHE_TAG_DELIMITER)
   })
 
   it('should construct with custom values', () => {
-    const config: Partial<ICacheTagConfig> = {
+    const config: Partial<IHttpCacheTagServerModuleConfig> = {
       headerKey: 'E-Cache',
       cacheableResponseCodes: [202, 204]
     }
 
     setupStandardTestBed(config)
     expect(TestBed.get(HttpCacheTagInterceptor)).toBeTruthy()
-    expect(TestBed.get(CACHE_TAG_CONFIG)).toEqual({
-      ...DEFAULT_CACHE_TAG_CONFIGURATION,
-      ...config
-    })
+    expect(TestBed.get(CACHE_TAG_HEADER_KEY)).toEqual('E-Cache')
+    expect(TestBed.get(CACHE_TAG_RESPONSE_CODES)).toEqual([202, 204])
   })
 
   it('should get express response header correcly', () => {
     setupStandardTestBed()
 
-    const config = TestBed.get(CACHE_TAG_CONFIG) as ICacheTagConfig
+    const headerKey = TestBed.get(CACHE_TAG_HEADER_KEY)
     const res = TestBed.get(RESPONSE)
     const spy = spyOn(res, 'header')
     const headerWriter = TestBed.get(CACHE_TAG_WRITE_HEADER_FACTORY) as IWriteResponseHeader
 
-    headerWriter(config.headerKey)('Video-2')(',')
+    headerWriter(headerKey)('Video-2')(',')
 
-    expect(spy).toHaveBeenCalledWith(DEFAULT_CACHE_TAG_CONFIGURATION.headerKey, 'Video-1,Video-2')
+    expect(spy).toHaveBeenCalledWith(DEFAULT_CACHE_TAG_HEADER_KEY, 'Video-1,Video-2')
   })
 
   it('should set express response headers', () => {
     setupStandardTestBed()
 
-    const config = TestBed.get(CACHE_TAG_CONFIG) as ICacheTagConfig
+    const headerKey = TestBed.get(CACHE_TAG_HEADER_KEY)
     const res = TestBed.get(RESPONSE)
     const spy = spyOn(res, 'getHeader')
     const headerWriter = TestBed.get(CACHE_TAG_WRITE_HEADER_FACTORY) as IWriteResponseHeader
 
-    headerWriter(config.headerKey)('Video-2')()
-    expect(spy).toHaveBeenCalledWith(DEFAULT_CACHE_TAG_CONFIGURATION.headerKey)
+    headerWriter(headerKey)('Video-2')()
+    expect(spy).toHaveBeenCalledWith(DEFAULT_CACHE_TAG_HEADER_KEY)
   })
 
   it('should set express response headers', () => {
     setupStandardTestBed()
 
-    const config = TestBed.get(CACHE_TAG_CONFIG) as ICacheTagConfig
+    const headerKey = TestBed.get(CACHE_TAG_HEADER_KEY)
     const res = TestBed.get(RESPONSE)
     const spy = spyOn(res, 'getHeader')
     const headerWriter = TestBed.get(CACHE_TAG_WRITE_HEADER_FACTORY) as IWriteResponseHeader
 
-    headerWriter(config.headerKey)('Video-2')('-')
-    expect(spy).toHaveBeenCalledWith(DEFAULT_CACHE_TAG_CONFIGURATION.headerKey)
+    headerWriter(headerKey)('Video-2')('-')
+    expect(spy).toHaveBeenCalledWith(DEFAULT_CACHE_TAG_HEADER_KEY)
   })
 
   it('should append strings correctly', () => {
--- projects/flosportsinc/ng-svg-transfer-state/browser/svg-transfer-state.browser.module.spec.ts
+++ projects/flosportsinc/ng-svg-transfer-state/browser/svg-transfer-state.browser.module.spec.ts
@@ -3,7 +3,7 @@ import { SVG_REQUEST_PATTERN_BASE, SVG_LOADER_BROWSER_CACHE_MAX_AGE } from '@flo
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import {
   SvgTransferStateBrowserModule, SvgTransferStateBrowserModuleConfig,
-  DEFAULT_CONFIG, DEFAULT_PATH
+  DEFAULT_PATH, DEFAULT_MAX_AGE
 } from './svg-transfer-state.browser.module'
 
 const setupTb = (config?: Partial<SvgTransferStateBrowserModuleConfig>) => {
@@ -26,15 +26,15 @@ describe(SvgTransferStateBrowserModule.name, () => {
     const patternBase = TestBed.get(SVG_REQUEST_PATTERN_BASE)
     const maxAge = TestBed.get(SVG_LOADER_BROWSER_CACHE_MAX_AGE)
 
-    expect(DEFAULT_PATH).toEqual(DEFAULT_CONFIG.dir as string)
-    expect(patternBase).toEqual(DEFAULT_CONFIG.dir)
-    expect(maxAge).toEqual(DEFAULT_CONFIG.cacheMaxAge)
+    expect(DEFAULT_PATH).toEqual(DEFAULT_PATH)
+    expect(patternBase).toEqual(DEFAULT_PATH)
+    expect(maxAge).toEqual(DEFAULT_MAX_AGE)
   })
 
   it('should default to a directory if config value is undefined', () => {
     setupTb({ dir: undefined })
 
     const patternBase = TestBed.get(SVG_REQUEST_PATTERN_BASE)
-    expect(patternBase).toEqual(DEFAULT_CONFIG.dir)
+    expect(patternBase).toEqual(DEFAULT_PATH)
   })
 })
