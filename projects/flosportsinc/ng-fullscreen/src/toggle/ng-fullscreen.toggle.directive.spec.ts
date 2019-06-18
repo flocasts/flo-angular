a44113c77049e3d7aec38321ef8e13c9b32d5f92
Patrick Michalina <patrickmichalina@mac.com>
2019-06-18 00:15:11 -0500

    coverage

--- projects/flosportsinc/ng-fullscreen/src/toggle/ng-fullscreen.toggle.directive.spec.ts
+++ projects/flosportsinc/ng-fullscreen/src/toggle/ng-fullscreen.toggle.directive.spec.ts
@@ -1,9 +1,10 @@
-import { Component, NgModule } from '@angular/core'
+import { Component, NgModule, ViewChild } from '@angular/core'
 import { FloFullscreenToggleModule } from './ng-fullscreen.toggle.module'
 import { FloClickToEnterFullscreenDirective, FloClickToExitFullscreenDirective } from './ng-fullscreen.toggle.directive'
 import { TestBed, async, fakeAsync, tick } from '@angular/core/testing'
 import { By } from '@angular/platform-browser'
 import { FloFullscreenService } from '../common/ng-fullscreen.service'
+import { DOCUMENT } from '@angular/common'
 
 @Component({
   selector: 'flo-test-component',
@@ -14,8 +15,17 @@ import { FloFullscreenService } from '../common/ng-fullscreen.service'
 })
 export class FloTestComponent { }
 
+@Component({
+  selector: 'flo-test-component-empty',
+  template: `<div id="container">
+    <button floClickToEnterFullscreen>ENTER</button>
+    <button floClickToExitFullscreen>EXIT</button>
+  </div>`
+})
+export class FloTestEmptyComponent { }
+
 @NgModule({
-  declarations: [FloTestComponent],
+  declarations: [FloTestComponent, FloTestEmptyComponent],
   imports: [FloFullscreenToggleModule],
   exports: [FloFullscreenToggleModule],
 })
@@ -52,9 +62,22 @@ describe(FloFullscreenToggleModule.name, () => {
       comp.detectChanges()
 
       sut.click()
-      tick(200)
+      tick(1)
 
       expect(spy).toHaveBeenCalledWith(container)
+    }))
+
+    it('should enter to fullscreen', fakeAsync(() => {
+      const comp = TestBed.createComponent(FloTestEmptyComponent)
+      const service = TestBed.get(FloFullscreenService) as FloFullscreenService
+      const sut = comp.debugElement.query(By.directive(FloClickToEnterFullscreenDirective)).nativeElement as HTMLButtonElement
+      const spy = spyOn(service, 'goFullscreen').and.callThrough()
+      comp.detectChanges()
+
+      sut.click()
+      tick(1)
+
+      expect(spy).toHaveBeenCalledWith(TestBed.get(DOCUMENT).body)
     }))
   })
 })
