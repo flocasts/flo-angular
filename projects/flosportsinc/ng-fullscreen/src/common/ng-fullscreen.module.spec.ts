6e76138242e42397ae3e6148adc0e8aad5fd262f
Patrick Michalina <patrickmichalina@mac.com>
2019-06-17 22:50:13 -0500

    initial tests

--- projects/flosportsinc/ng-fullscreen/src/common/ng-fullscreen.module.spec.ts
+++ projects/flosportsinc/ng-fullscreen/src/common/ng-fullscreen.module.spec.ts
@@ -1,34 +1,43 @@
-import { NgModule, Component } from '@angular/core'
 import { TestBed } from '@angular/core/testing'
-import { FloFullscreenService } from './ng-fullscreen.service'
 import { FloFullscreenCommonModule } from './ng-fullscreen.module'
+import {
+  FS_FULLSCREEN_REQUEST_EVENTS, FS_FULLSCREEN_EXIT_EVENTS, FS_FULLSCREEN_CHANGE_EVENTS,
+  FS_FULLSCREEN_ELEMENT_ERROR_EVENTS, FS_FULLSCREEN_ELEMENT, FS_FULLSCREEN_ENABLED
+} from './ng-fullscreen.tokens'
+import {
+  DEFAULT_FS_FULLSCREEN_REQUEST_EVENTS, DEFAULT_FS_FULLSCREEN_EXIT_EVENTS,
+  DEFAULT_FS_FULLSCREEN_ELEMENT_ERROR_EVENTS, DEFAULT_FS_FULLSCREEN_ELEMENT,
+  DEFAULT_FS_FULLSCREEN_CHANGE_EVENTS, DEFAULT_FS_FULLSCREEN_ENABLED
+} from './ng-fullscreen.tokens.defaults'
 
-@Component({
-  selector: 'flo-test-component',
-  template: `<span floFullscreen>I indicate fullscreen CTA</span>`
-})
-export class FloTestComponent { }
+// @Component({
+//   selector: 'flo-test-component',
+//   template: `<span>I indicate fullscreen CTA</span>`
+// })
+// export class FloTestComponent { }
 
-@NgModule({
-  declarations: [FloTestComponent],
-  imports: [FloFullscreenCommonModule],
-  exports: [FloFullscreenCommonModule, FloTestComponent],
-})
-export class FloFullscreenTestModule { }
+// @NgModule({
+//   declarations: [FloTestComponent],
+//   imports: [FloFullscreenCommonModule],
+//   exports: [FloFullscreenCommonModule, FloTestComponent],
+// })
+// export class FloFullscreenTestModule { }
 
 describe(FloFullscreenCommonModule.name, () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [FloFullscreenTestModule]
-    }).compileComponents()
+      imports: [FloFullscreenCommonModule]
+    })
   })
 
-  it('should compile', () => {
-    expect(TestBed.get(FloFullscreenService)).toBeTruthy()
-  })
+  it('should use module defaults', () => {
+    expect(TestBed.get(FS_FULLSCREEN_REQUEST_EVENTS)).toEqual(DEFAULT_FS_FULLSCREEN_REQUEST_EVENTS)
 
-  it('should compile', () => {
-    expect(TestBed.createComponent(FloTestComponent)).toBeTruthy()
+    expect(TestBed.get(FS_FULLSCREEN_EXIT_EVENTS)).toEqual(DEFAULT_FS_FULLSCREEN_EXIT_EVENTS)
+    expect(TestBed.get(FS_FULLSCREEN_CHANGE_EVENTS)).toEqual(DEFAULT_FS_FULLSCREEN_CHANGE_EVENTS)
+    expect(TestBed.get(FS_FULLSCREEN_ELEMENT_ERROR_EVENTS)).toEqual(DEFAULT_FS_FULLSCREEN_ELEMENT_ERROR_EVENTS)
+    expect(TestBed.get(FS_FULLSCREEN_ELEMENT)).toEqual(DEFAULT_FS_FULLSCREEN_ELEMENT)
+    expect(TestBed.get(FS_FULLSCREEN_ENABLED)).toEqual(DEFAULT_FS_FULLSCREEN_ENABLED)
   })
 })
 
--- projects/flosportsinc/ng-fullscreen/src/common/ng-fullscreen.service.spec.ts
+++ projects/flosportsinc/ng-fullscreen/src/common/ng-fullscreen.service.spec.ts
@@ -1,8 +1,11 @@
 import { TestBed } from '@angular/core/testing'
 import { FloFullscreenService } from './ng-fullscreen.service'
+import { FloFullscreenCommonModule } from './ng-fullscreen.module'
 
 describe(FloFullscreenService.name, () => {
-  beforeEach(() => TestBed.configureTestingModule({}))
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [FloFullscreenCommonModule]
+  }))
 
   it('should be created', () => {
     expect(TestBed.get(FloFullscreenService)).toBeTruthy()
