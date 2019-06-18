4be527f6feea40dfe3da613dbb64d6a0044d8398
Patrick Michalina <patrickmichalina@mac.com>
2019-06-18 11:05:19 -0500

    tests

--- projects/flosportsinc/ng-fullscreen/src/common/ng-fullscreen.service.spec.ts
+++ projects/flosportsinc/ng-fullscreen/src/common/ng-fullscreen.service.spec.ts
@@ -1,13 +1,61 @@
-import { TestBed } from '@angular/core/testing'
+import { TestBed, async } from '@angular/core/testing'
 import { FloFullscreenService } from './ng-fullscreen.service'
 import { FloFullscreenCommonModule } from './ng-fullscreen.module'
+import { PLATFORM_ID } from '@angular/core';
+import { take, skip } from 'rxjs/operators';
 
 describe(FloFullscreenService.name, () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [FloFullscreenCommonModule]
-  }))
+  // tslint:disable-next-line: no-let
+  let service: FloFullscreenService
 
-  it('should be created', () => {
-    expect(TestBed.get(FloFullscreenService)).toBeTruthy()
+  const setService = () => service = TestBed.get(FloFullscreenService)
+
+  describe('when on platform browser', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [FloFullscreenCommonModule],
+        providers: [{ provide: PLATFORM_ID, useValue: 'browser' }]
+      })
+      setService()
+    })
+
+    afterEach(() => TestBed.resetTestingModule())
+
+    it('should be created', () => {
+      expect(service).toBeTruthy()
+    })
+
+    it('should return false true fullscreenIsSupported', async(() => {
+      setService()
+      expect(service.fullscreenIsSupported()).toEqual(true)
+    }))
+  })
+
+  describe('when on platform server', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [FloFullscreenCommonModule],
+        providers: [{ provide: PLATFORM_ID, useValue: 'server' }]
+      })
+      setService()
+    })
+
+    afterEach(() => TestBed.resetTestingModule())
+
+    it('should be created', () => {
+      expect(service).toBeTruthy()
+    })
+
+    it('should return false from canGoFullscreen', async(() => {
+      setService()
+      service.canGoFullscreen$.subscribe(val => {
+        expect(val).toEqual(false)
+      })
+    }))
+
+    it('should return false from fullscreenIsSupported', async(() => {
+      setService()
+      expect(service.fullscreenIsSupported()).toEqual(false)
+    }))
   })
 })
--- projects/flosportsinc/ng-fullscreen/src/switch/ng-fullscreen.switch.directive.spec.ts
+++ projects/flosportsinc/ng-fullscreen/src/switch/ng-fullscreen.switch.directive.spec.ts
@@ -1,22 +1,64 @@
-import { Component } from '@angular/core'
+import { Component, NgModule } from '@angular/core'
 import { TestBed } from '@angular/core/testing'
 import { FloFullscreenDirective } from './ng-fullscreen.switch.directive'
+import { FloFullscreenSwitchModule } from './ng-fullscreen.switch.module'
+import { By } from '@angular/platform-browser'
+import { FloFullscreenService } from '../common/ng-fullscreen.service'
+import { of } from 'rxjs'
 
-// @Component({
-//   selector: 'flo-test-component',
-//   template: `<span>I indicate fullscreen CTA</span>`
-// })
-// export class FloTestComponent { }
+@Component({
+  selector: 'flo-test-component',
+  template: `<div id="container">
+    <button *floIfNotFullscreen>ENTER</button>
+    <button *floIfFullscreen>EXIT</button>
+  </div>`
+})
+export class FloTestComponent { }
 
-// @NgModule({
-//   declarations: [FloTestComponent],
-//   imports: [FloFullscreenCommonModule],
-//   exports: [FloFullscreenCommonModule, FloTestComponent],
-// })
-// export class FloFullscreenTestModule { }
+@NgModule({
+  declarations: [FloTestComponent],
+  imports: [FloFullscreenSwitchModule],
+  exports: [FloFullscreenSwitchModule, FloTestComponent],
+})
+export class FloFullscreenTestModule { }
 
+const createSut = () => {
+  const sut = TestBed.createComponent(FloTestComponent)
+  sut.detectChanges()
+  return sut
+}
 
 describe(FloFullscreenDirective.name, () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FloFullscreenTestModule]
+    })
+  })
+
   it('should compile', () => {
+    expect(createSut()).toBeTruthy()
+  })
+
+  it('should show ifNotFullscreen element', () => {
+    const sut = createSut()
+    const container = sut.debugElement.query(By.css('#container'))
+    const tag = container.query(By.css('button'))
+    expect(tag.nativeElement.innerText).toEqual('ENTER')
+  })
+
+  it('should show floIfFullscreen element', () => {
+    TestBed.resetTestingModule()
+    TestBed.configureTestingModule({
+      imports: [FloFullscreenTestModule],
+      providers: [
+        { provide: FloFullscreenService, useValue: { fullscreen$: of(true) } }
+      ]
+    })
+
+    const sut = createSut()
+    sut.detectChanges()
+    const container = sut.debugElement.query(By.css('#container'))
+    const tag = container.query(By.css('button'))
+    expect(tag.nativeElement.innerText).toEqual('EXIT')
   })
 })
