a54b0ab4b9babdac0b37fa6866e3494e287f026a
Patrick Michalina <patrickmichalina@mac.com>
2019-06-18 00:03:37 -0500

    test

--- src/app/fullscreen/fullscreen.component.spec.ts
+++ src/app/fullscreen/fullscreen.component.spec.ts
@@ -1,9 +1,11 @@
 import { async, TestBed } from '@angular/core/testing'
 import { FullscreenComponent } from './fullscreen.component'
+import { FloFullscreenModule } from 'projects/flosportsinc/ng-fullscreen/src/ng-fullscreen.module'
 
 describe('FullscreenComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [FloFullscreenModule],
       declarations: [FullscreenComponent]
     }).compileComponents()
   }))
