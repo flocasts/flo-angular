1ae0f752ef1709ef05f19571f0b6eb62900f3b90
Patrick Michalina <patrickmichalina@mac.com>
2019-06-19 17:16:45 -0500

    wip

--- projects/flosportsinc/ng-video-player/controls/video-player-play-control/video-player-play-control.directive.ts
+++ projects/flosportsinc/ng-video-player/controls/video-player-play-control/video-player-play-control.directive.ts
@@ -1,5 +1,6 @@
-import { Directive, Input, HostListener, ElementRef, Inject } from '@angular/core'
-import { VIDEO_PLAYER_CONTROLS_PLAY_FUNC, Te } from './video-player-play-control.tokens'
+import { Directive, Input, HostListener, ElementRef, Inject, PLATFORM_ID } from '@angular/core'
+import { VIDEO_PLAYER_CONTROLS_PLAY_FUNC, Te as PlayControlFunction } from './video-player-play-control.tokens'
+import { isPlatformBrowser } from '@angular/common'
 
 // tslint:disable: no-if-statement
 
@@ -7,14 +8,16 @@ import { VIDEO_PLAYER_CONTROLS_PLAY_FUNC, Te } from './video-player-play-control
   selector: '[floVpcPlay]',
 })
 export class FloVideoPlayerPlayControlDirective<TMeta = any> {
-  constructor(private elmRef: ElementRef<HTMLElement>, @Inject(VIDEO_PLAYER_CONTROLS_PLAY_FUNC) private func: Te) { }
+  constructor(private elmRef: ElementRef<HTMLElement>,
+    @Inject(VIDEO_PLAYER_CONTROLS_PLAY_FUNC) private func: PlayControlFunction,
+    @Inject(PLATFORM_ID) private platformId: string) { }
 
   @Input() readonly floVpcPlay?: HTMLVideoElement
   @Input() readonly floVpcPlayMeta?: TMeta
 
   @HostListener('click', [])
   click() {
-    if (this.floVpcPlay && this.floVpcPlay instanceof HTMLVideoElement) {
+    if (this.floVpcPlay && isPlatformBrowser(this.platformId) && this.floVpcPlay instanceof HTMLVideoElement) {
       this.func(this.floVpcPlay, this.elmRef, this.floVpcPlayMeta)
     }
   }
