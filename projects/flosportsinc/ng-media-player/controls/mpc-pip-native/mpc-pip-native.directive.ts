18544193e8770b989d1aaf665d9a54af2409a410
Patrick Michalina <patrickmichalina@mac.com>
2019-06-24 15:05:21 -0500

    wip

--- projects/flosportsinc/ng-media-player/controls/mpc-pip-native/mpc-pip-native.directive.ts
+++ projects/flosportsinc/ng-media-player/controls/mpc-pip-native/mpc-pip-native.directive.ts
@@ -1,22 +1,16 @@
 import { Directive, Input, HostListener, Inject, PLATFORM_ID } from '@angular/core'
 import { FloMediaPlayerControlDirectiveBase, coerceInputToBoolean } from '../mpc-base.directive'
-import { DOCUMENT } from '@angular/common'
+import { FloPictureInPictureNativeService } from './mpc-pip-native.service'
 
 // tslint:disable: no-if-statement
 // tslint:disable: readonly-keyword
 // tslint:disable: no-object-mutation
 
-export enum SAFARI_PIP_STATES {
-  PIP = 'picture-in-picture',
-  INLINE = 'inline'
-}
-
 @Directive({
   selector: '[floMpc][floMpcPipNativeEnter]'
 })
 export class FloMediaPlayerControlPipNativeEnterDirective<TMeta = any> extends FloMediaPlayerControlDirectiveBase<TMeta> {
-  constructor(@Inject(PLATFORM_ID) protected platformId: string,
-    @Inject(DOCUMENT) private doc: any) {
+  constructor(private ps: FloPictureInPictureNativeService, @Inject(PLATFORM_ID) protected platformId: string) {
     super(platformId)
   }
 
@@ -30,31 +24,14 @@ export class FloMediaPlayerControlPipNativeEnterDirective<TMeta = any> extends F
     this._floMpcPipNativeEnter = coerceInputToBoolean(val)
   }
 
-  readonly useChromeVariant = () => 'pictureInPictureEnabled' in this.doc
-  readonly useSafariVariant = (video: any) => video.webkitSupportsPresentationMode && typeof video.webkitSetPresentationMode === 'function'
-
-  readonly enterPip = (video: any) => {
-    if (this.useChromeVariant()) {
-      video.requestPictureInPicture()
-    } else if (this.useSafariVariant(video)) {
-      video.webkitSetPresentationMode(SAFARI_PIP_STATES.PIP)
-    }
-  }
-
-  readonly isInPip = (video: any) => {
-    if (this.useChromeVariant()) {
-      return this.doc.pictureInPictureElement === video
-    } else if (this.useSafariVariant(video)) {
-      return video.webkitPresentationMode === SAFARI_PIP_STATES.PIP
-    }
-  }
-
   @HostListener('click')
   click() {
     if (!this.floMpcPipNativeEnter) { return }
-    this.maybeMediaElement().tapSome(ve => {
-      this.enterPip(ve)
-    })
+    this.maybeMediaElement()
+      .filter(a => a instanceof HTMLVideoElement)
+      .tapSome((ve: HTMLVideoElement) => {
+        this.ps.enterPip(ve)
+      })
   }
 }
 
@@ -64,11 +41,25 @@ export class FloMediaPlayerControlPipNativeEnterDirective<TMeta = any> extends F
 export class FloMediaPlayerControlPipNativeExitDirective<TMeta = any> extends FloMediaPlayerControlDirectiveBase<TMeta> {
   private _floMpcPipNativeExit?: string | boolean
 
+  constructor(private ps: FloPictureInPictureNativeService, @Inject(PLATFORM_ID) platformId: string) {
+    super(platformId)
+  }
+
   @Input()
   get floMpcPipNativeExit() {
     return this._floMpcPipNativeExit
   }
   set floMpcPipNativeExit(val: any) {
     this._floMpcPipNativeExit = coerceInputToBoolean(val)
+  }
+
+  @HostListener('click')
+  click() {
+    if (!this.floMpcPipNativeExit) { return }
+    this.maybeMediaElement()
+      .filter(a => a instanceof HTMLVideoElement)
+      .tapSome((ve: HTMLVideoElement) => {
+        this.ps.exitPip(ve)
+      })
   }
 }
--- projects/flosportsinc/ng-media-player/controls/mpc-pip-native/mpc-pip-native.service.ts
+++ projects/flosportsinc/ng-media-player/controls/mpc-pip-native/mpc-pip-native.service.ts
@@ -1,12 +1,32 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core'
 import { DOCUMENT, isPlatformServer } from '@angular/common'
 
+// tslint:disable: no-if-statement
+
 export interface IFloPictureInPictureNativeService {
-  readonly platformSupportsPip: () => boolean
-  readonly enterPip: (media: HTMLMediaElement) => void
-  readonly exitPip: (media: HTMLMediaElement) => void
-  readonly getPipElement: (media: HTMLMediaElement) => HTMLElement
+  readonly platformSupportsPip: (mediaElement: HTMLMediaElement | SafariPipStandardHTMLMediaElement) => boolean
+  readonly enterPip: (video: HTMLVideoElement) => void
+  readonly exitPip: (video: HTMLVideoElement) => void
+  readonly getPipElement: (video: HTMLVideoElement) => HTMLMediaElement | undefined
   readonly isPipActive: () => boolean
+}
+
+export enum SAFARI_PIP_STATES {
+  PIP = 'picture-in-picture',
+  INLINE = 'inline'
+}
+
+export type WebPipStandardHTMLMediaElement = HTMLMediaElement & {
+  readonly disablePictureInPicture: boolean
+  readonly requestPictureInPicture: () => Promise<any>
+}
+export type WebPipStandardHTMLDocument = HTMLDocument & {
+  readonly pictureInPictureEnabled: boolean
+  readonly exitPictureInPicture: () => Promise<any>
+}
+export type SafariPipStandardHTMLMediaElement = HTMLMediaElement & {
+  readonly webkitSupportsPresentationMode: boolean
+  readonly webkitSetPresentationMode: (val: SAFARI_PIP_STATES) => void
 }
 
 @Injectable({ providedIn: 'root' })
@@ -16,9 +36,48 @@ export class FloPictureInPictureNativeService implements IFloPictureInPictureNat
     @Inject(PLATFORM_ID) private platformId: string
   ) { }
 
-  readonly platformSupportsPip: () => false
-  readonly enterPip: () => void
-  readonly exitPip: () => void
-  readonly isPipActive: () => false
-  readonly getPipElement: () => HTMLElement
+  // https://wicg.github.io/picture-in-picture
+  private readonly useWebStandardVariant = (media?: WebPipStandardHTMLMediaElement) => {
+    const docSupport = 'pictureInPictureEnabled' in this.doc
+
+    return media
+      ? docSupport && !media.disablePictureInPicture
+      : docSupport
+  }
+
+  // tslint:disable-next-line: max-line-length
+  // https://developer.apple.com/library/archive/releasenotes/General/WhatsNewInSafari/Articles/Safari_9_0.html#//apple_ref/doc/uid/TP40014305-CH9-SW18
+  private readonly useSafariStandardVariant = (media: SafariPipStandardHTMLMediaElement) =>
+    media.webkitSupportsPresentationMode && typeof media.webkitSetPresentationMode === 'function'
+
+
+  readonly enterPip = (media: HTMLMediaElement) => {
+    if (this.useWebStandardVariant(media as WebPipStandardHTMLMediaElement)) {
+      (media as WebPipStandardHTMLMediaElement).requestPictureInPicture().catch(console.error)
+    } else if (this.useSafariStandardVariant(media as SafariPipStandardHTMLMediaElement)) {
+      (media as SafariPipStandardHTMLMediaElement).webkitSetPresentationMode(SAFARI_PIP_STATES.PIP)
+    }
+  }
+
+  readonly exitPip = (media: HTMLMediaElement) => {
+    if (this.useWebStandardVariant(media as WebPipStandardHTMLMediaElement)) {
+      (this.doc as WebPipStandardHTMLDocument).exitPictureInPicture().catch(er => console.log(er))
+    } else if (this.useSafariStandardVariant(media as SafariPipStandardHTMLMediaElement)) {
+      (media as SafariPipStandardHTMLMediaElement).webkitSetPresentationMode(SAFARI_PIP_STATES.INLINE)
+    }
+  }
+
+  readonly getPipElement = (): HTMLMediaElement | undefined => this.useWebStandardVariant()
+    ? this.doc.pictureInPictureElement
+    : Array.from((this.doc as HTMLDocument).querySelectorAll('video'))
+      .filter(video => (video as any).webkitPresentationMode === SAFARI_PIP_STATES.PIP)
+      .reverse()
+      .pop()
+
+  readonly platformSupportsPip =
+    (mediaElement: HTMLMediaElement) =>
+      this.useWebStandardVariant() || this.useSafariStandardVariant(mediaElement as SafariPipStandardHTMLMediaElement)
+
+  readonly isPipActive = () => this.getPipElement() !== undefined
+  readonly canEnterPip = () => false
 }
