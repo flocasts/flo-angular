926ca410a1383508ad795ffb26066676e3a4e1a5
Patrick Michalina <patrickmichalina@mac.com>
2019-07-23 23:05:34 -0500

    wip

--- projects/flosportsinc/ng-media-player/controls/mp-unmute/mp-unmute.tokens.ts
+++ projects/flosportsinc/ng-media-player/controls/mp-unmute/mp-unmute.tokens.ts
@@ -5,8 +5,12 @@ export const MEDIA_PLAYER_CONTROLS_UNMUTE_FUNC = new InjectionToken('fs.mp.ctrl.
 
 export function defaultUnmuteFactoryFunction(): UnmuteControlFunction {
   const lambda = (mediaRef: HTMLMediaElement, _elmRef: ElementRef<HTMLElement>, _meta: any) => {
-    // tslint:disable-next-line: no-object-mutation
+    // tslint:disable: no-object-mutation
     mediaRef.muted = false
+    // tslint:disable-next-line: no-if-statement
+    if (mediaRef.volume === 0) {
+      mediaRef.volume = 1
+    }
   }
   return lambda
 }
