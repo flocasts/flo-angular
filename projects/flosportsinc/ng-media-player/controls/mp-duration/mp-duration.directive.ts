85def402a53723b183ab76d00ffe649cfdc74502
Patrick Michalina <patrickmichalina@mac.com>
2019-07-24 00:29:42 -0500

    wip

--- projects/flosportsinc/ng-media-player/controls/mp-duration/mp-duration.directive.ts
+++ projects/flosportsinc/ng-media-player/controls/mp-duration/mp-duration.directive.ts
@@ -1,7 +1,8 @@
 import { Directive, Input, Inject, ChangeDetectorRef, PLATFORM_ID, ElementRef } from '@angular/core'
 import { FloMediaPlayerControlDirectiveBase } from '../mp-base.directive'
-import { fromEvent } from 'rxjs'
+import { fromEvent, combineLatest } from 'rxjs'
 import { isPlatformServer } from '@angular/common'
+import { map, distinctUntilChanged } from 'rxjs/operators'
 
 // tslint:disable: no-if-statement
 // tslint:disable: readonly-keyword
@@ -25,20 +26,43 @@ export class FloMediaPlayerDurationControlDirective<TMeta = any> extends FloMedi
     this._floMpDuration = val
   }
 
+  private formatdate = (secondsInt: number) => {
+    const hours = Math.floor(secondsInt / 3600)
+    const minutes = Math.floor(secondsInt / 60) % 60
+    const seconds = secondsInt % 60
+    return {
+      hours,
+      minutes,
+      seconds,
+      formatted: [hours, minutes, seconds]
+        .map(v => v < 10 ? '0' + v : v)
+        .filter((v, i) => v !== '00' || i > 0)
+        .join(':')
+    }
+  }
+
   // tslint:disable-next-line: use-life-cycle-interface
   ngOnInit() {
     if (isPlatformServer(this.platformId)) { return }
+
     if (this.mediaRef) {
-      fromEvent(this.mediaRef, 'durationchange').subscribe(evt => {
-        const media = evt.target as HTMLMediaElement
-        const secondsInt = parseInt('' + media.duration, 10)
-        const hours = Math.floor(secondsInt / 3600)
-        const minutes = Math.floor(secondsInt / 60) % 60
-        const seconds = secondsInt % 60
-        const z = [hours, minutes, seconds]
-          .map(v => v < 10 ? '0' + v : v)
-          .filter((v, i) => v !== '00' || i > 0)
-          .join(':')
+      const timeupdate = fromEvent(this.mediaRef, 'timeupdate').pipe(
+        map(evt => Math.floor((evt.target as HTMLMediaElement).currentTime)),
+        distinctUntilChanged()
+      )
+      const durationchange = fromEvent(this.mediaRef, 'durationchange').pipe(
+        map(evt => parseInt('' + (evt.target as HTMLMediaElement).duration, 10))
+      )
+
+      combineLatest(timeupdate, durationchange).pipe(
+        map(res => {
+          return {
+            current: this.formatdate(res[0]),
+            duratiion: this.formatdate(res[1])
+          }
+        })
+      ).subscribe(res => {
+        console.log(res)
       })
     }
   }
