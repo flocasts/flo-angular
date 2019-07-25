c9fec5dc285427215675e993c7e0433259daed26
Patrick Michalina <patrickmichalina@mac.com>
2019-07-25 16:36:41 -0500

    wip

--- src/app/fetch-fill/fetch-fill.component.spec.ts
+++ src/app/fetch-fill/fetch-fill.component.spec.ts
@@ -1,25 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { FetchFillComponent } from './fetch-fill.component';
+import { async, TestBed } from '@angular/core/testing'
+import { FetchFillComponent } from './fetch-fill.component'
 
 describe('FetchFillComponent', () => {
-  let component: FetchFillComponent;
-  let fixture: ComponentFixture<FetchFillComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ FetchFillComponent ]
     })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(FetchFillComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    .compileComponents()
+  }))
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    const comp = TestBed.createComponent(FetchFillComponent)
+    comp.detectChanges()
+    expect(comp).toBeTruthy()
+  })
+})
