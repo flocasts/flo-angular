ffee65be77feb33a0eaf61225505c06604dd9455
Patrick Michalina <patrickmichalina@mac.com>
2019-07-25 16:16:41 -0500

    refact

---
+++ src/app/flo.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core'
+import { FloMseModule } from '@flosportsinc/ng-media-source-extensions'
+import { FloWindowModule } from '@flosportsinc/ng-window'
+import { FloVideoAutoplayModule } from '@flosportsinc/ng-video-autoplay'
+import { FloVideoEventsModule } from '@flosportsinc/ng-video-events'
+import { FloGridListModule } from '@flosportsinc/ng-grid-list'
+import { FloNodeEnvTransferModule } from '@flosportsinc/ng-env-transfer-state'
+import { FloFullscreenModule } from '@flosportsinc/ng-fullscreen'
+import { SvgTransferStateModule } from '@flosportsinc/ng-svg-transfer-state'
+
+@NgModule({
+  imports: [
+    FloVideoEventsModule,
+    FloVideoAutoplayModule,
+    FloFullscreenModule,
+    FloMseModule,
+    FloWindowModule,
+    SvgTransferStateModule,
+    FloGridListModule.config({
+      count: 2,
+      autoSelectNextEmptyOnCountChange: true
+    }),
+    FloNodeEnvTransferModule.config({
+      useValues: {
+        manuallyShared: 'yay!'
+      }
+    })
+  ],
+  exports: [
+    FloVideoEventsModule,
+    FloVideoAutoplayModule,
+    FloFullscreenModule,
+    FloMseModule,
+    FloWindowModule,
+    SvgTransferStateModule,
+    FloGridListModule,
+    FloNodeEnvTransferModule
+  ]
+})
+export class FloModule { }
