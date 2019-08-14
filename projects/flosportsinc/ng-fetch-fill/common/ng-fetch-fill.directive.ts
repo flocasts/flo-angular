e16ce911fbf0a02cdbf69d08b31b42b4644e1c4e
Patrick Michalina <patrickmichalina@mac.com>
2019-08-14 15:18:04 -0500

    feat(fetch-fill): add debounceTime input property (#213)

--- projects/flosportsinc/ng-fetch-fill/common/ng-fetch-fill.directive.ts
+++ projects/flosportsinc/ng-fetch-fill/common/ng-fetch-fill.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ContentChildren, AfterContentInit, Input, QueryList, Inject, Optional, OnDestroy } from '@angular/core'
-import { LOADERS, ILoader } from './ng-fetch-fill.tokens'
+import { LOADERS, ILoader, FS_FETCH_FILL_DEBOUNCE } from './ng-fetch-fill.tokens'
 import { FloFetchFillItemDirective } from './ng-fetch-fill-item.directive'
-import { filter, map, takeUntil, flatMap } from 'rxjs/operators'
+import { filter, map, takeUntil, flatMap, debounceTime } from 'rxjs/operators'
 import { Subject } from 'rxjs'
 
 // tslint:disable: readonly-keyword
@@ -14,12 +14,21 @@ type Query<TInItem, TOutItem> = QueryList<FloFetchFillItemDirective<TInItem, TOu
   selector: '[floFetchFill]'
 })
 export class FloFetchFillDirective<TInItem, TOutItem, TInError, TOutError> implements AfterContentInit, OnDestroy {
-  constructor(@Optional() @Inject(LOADERS) private _loaders: ILoader<TInItem, TOutItem, TInError, TOutError>[]) { }
+  constructor(@Optional() @Inject(LOADERS) private _loaders: ILoader<TInItem, TOutItem, TInError, TOutError>[],
+    @Inject(FS_FETCH_FILL_DEBOUNCE) private _debounceTime: number) { }
 
   private ngOnDestroy$ = new Subject()
 
   get loaders() {
     return this._loaders || []
+  }
+
+  @Input()
+  get debounceTime() {
+    return this._debounceTime
+  }
+  set debounceTime(val: any) {
+    this._debounceTime = typeof val === 'number' ? val : this._debounceTime
   }
 
   @Input() readonly floFetchFill?: string
@@ -31,6 +40,7 @@ export class FloFetchFillDirective<TInItem, TOutItem, TInError, TOutError> imple
     if (!loader) { return }
 
     this.fillItems.changes.pipe(
+      debounceTime(this.debounceTime),
       map((a: Query<TInItem, TOutItem>) => a.toArray()),
       takeUntil(this.ngOnDestroy$),
       flatMap(items => loader.func(items.map(a => a.input)).pipe(
