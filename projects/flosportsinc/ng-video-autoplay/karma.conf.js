2c27192a1d8397256cd40490485a7f93696905fd
Patrick Michalina <patrickmichalina@mac.com>
2020-02-17 20:41:36 -0600

    test: reduce coverage requirement

--- projects/flosportsinc/ng-video-autoplay/karma.conf.js
+++ projects/flosportsinc/ng-video-autoplay/karma.conf.js
@@ -1,2 +1,7 @@
 const config = require('../../../src/karma.conf.shared')
-module.exports = config('../coverage/ng-video-autoplay')()()
+module.exports = config('../coverage/ng-video-autoplay')()({
+  statements: 96,
+  lines: 96,
+  branches: 96,
+  functions: 96
+})
