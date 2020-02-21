146ba120f2dd1c76fdb30ae1c419048ff2ccc389
William Reynolds <william.reynolds@flosports.tv>
2020-02-21 15:22:53 -0600

    tests(lazy-load): correct test bed setup

--- src/app/lazy-load/lazy-load.component.spec.ts
+++ src/app/lazy-load/lazy-load.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { LazyLoadComponent } from './lazy-load.component'
+import {FloLazyLoadModule} from '@flosportsinc/ng-lazy-load'
+import {CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core'
+import {SharedTestingModule} from '../shared.testing.module'
 
 describe('LazyLoadComponent', () => {
   // tslint:disable-next-line:no-let
@@ -10,7 +13,12 @@ describe('LazyLoadComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LazyLoadComponent ]
+      imports: [
+        FloLazyLoadModule,
+        SharedTestingModule
+      ],
+      declarations: [ LazyLoadComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     })
     .compileComponents()
   }))
